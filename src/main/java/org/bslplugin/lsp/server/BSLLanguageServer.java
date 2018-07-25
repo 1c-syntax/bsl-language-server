618938c3f31364873e1437c9517d0cb344bdd0ff
Nikita Gryzlov <nixel2007@gmail.com>
2018-07-25 23:15:28 +0300

    Доустановка обязательной "возможности" сервера

--- src/main/java/org/bslplugin/lsp/server/BSLLanguageServer.java
+++ src/main/java/org/bslplugin/lsp/server/BSLLanguageServer.java
@@ -5,54 +5,60 @@ import org.eclipse.lsp4j.CompletionOptions;
 import org.eclipse.lsp4j.InitializeParams;
 import org.eclipse.lsp4j.InitializeResult;
 import org.eclipse.lsp4j.ServerCapabilities;
-import org.eclipse.lsp4j.services.*;
+import org.eclipse.lsp4j.TextDocumentSyncKind;
+import org.eclipse.lsp4j.services.LanguageClient;
+import org.eclipse.lsp4j.services.LanguageClientAware;
+import org.eclipse.lsp4j.services.LanguageServer;
+import org.eclipse.lsp4j.services.TextDocumentService;
+import org.eclipse.lsp4j.services.WorkspaceService;
 
 import java.util.concurrent.CompletableFuture;
 
 public class BSLLanguageServer implements LanguageServer, LanguageClientAware {
 
-    private LanguageClient client;
-    private TextDocumentService textDocumentService;
-    private WorkspaceService workspaceService;
+  private LanguageClient client;
+  private TextDocumentService textDocumentService;
+  private WorkspaceService workspaceService;
 
-    public BSLLanguageServer() {
-        textDocumentService = new BSLTextDocumentService();
-        workspaceService = new BSLWorkspaceService();
-    }
+  public BSLLanguageServer() {
+    textDocumentService = new BSLTextDocumentService();
+    workspaceService = new BSLWorkspaceService();
+  }
 
-    @Override
-    public CompletableFuture<InitializeResult> initialize(InitializeParams params) {
+  @Override
+  public CompletableFuture<InitializeResult> initialize(InitializeParams params) {
 
-        ServerCapabilities capabilities = new ServerCapabilities();
-        capabilities.setCompletionProvider(new CompletionOptions(true, Lists.newArrayList(".")));
+    ServerCapabilities capabilities = new ServerCapabilities();
+    capabilities.setTextDocumentSync(TextDocumentSyncKind.Full);
+    capabilities.setCompletionProvider(new CompletionOptions(false, Lists.newArrayList(".")));
 
-        InitializeResult result = new InitializeResult(capabilities);
+    InitializeResult result = new InitializeResult(capabilities);
 
-        return CompletableFuture.completedFuture(result);
-    }
+    return CompletableFuture.completedFuture(result);
+  }
 
-    @Override
-    public CompletableFuture<Object> shutdown() {
-        return CompletableFuture.completedFuture(Boolean.TRUE);
-    }
+  @Override
+  public CompletableFuture<Object> shutdown() {
+    return CompletableFuture.completedFuture(Boolean.TRUE);
+  }
 
-    @Override
-    public void exit() {
+  @Override
+  public void exit() {
 
-    }
+  }
 
-    @Override
-    public TextDocumentService getTextDocumentService() {
-        return textDocumentService;
-    }
+  @Override
+  public TextDocumentService getTextDocumentService() {
+    return textDocumentService;
+  }
 
-    @Override
-    public WorkspaceService getWorkspaceService() {
-        return workspaceService;
-    }
+  @Override
+  public WorkspaceService getWorkspaceService() {
+    return workspaceService;
+  }
 
-    @Override
-    public void connect(LanguageClient client) {
-        this.client = client;
-    }
+  @Override
+  public void connect(LanguageClient client) {
+    this.client = client;
+  }
 }
