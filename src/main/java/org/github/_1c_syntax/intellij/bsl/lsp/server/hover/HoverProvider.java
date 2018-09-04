123437996aa1e5e28b6d14b4acac02a15ed90431
Nikita Gryzlov <nixel2007@gmail.com>
2018-09-04 13:52:27 +0300

    Лицензия

--- src/main/java/org/github/_1c_syntax/intellij/bsl/lsp/server/hover/HoverProvider.java
+++ src/main/java/org/github/_1c_syntax/intellij/bsl/lsp/server/hover/HoverProvider.java
@@ -1,3 +1,24 @@
+/**
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018
+ * Alexey Sosnoviy <labotamy@yandex.ru>, Nikita Gryzlov <nixel2007@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
 package org.github._1c_syntax.intellij.bsl.lsp.server.hover;
 
 import org.antlr.v4.runtime.Token;
