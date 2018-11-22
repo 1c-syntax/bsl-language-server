f4249489cb217b616ec94a52d8b28ff733e73f86
Nikita Gryzlov <nixel2007@gmail.com>
2018-11-22 17:12:04 +0700

    Хэлпер для создания Range

--- src/main/java/org/github/_1c_syntax/intellij/bsl/lsp/server/utils/RangeHelper.java
+++ src/main/java/org/github/_1c_syntax/intellij/bsl/lsp/server/utils/RangeHelper.java
@@ -1,10 +1,24 @@
 package org.github._1c_syntax.intellij.bsl.lsp.server.utils;
 
+import org.antlr.v4.runtime.Token;
 import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.Range;
 
 public class RangeHelper {
   public static Range newRange(int startLine, int startChar, int endLine, int endChar) {
     return new Range(new Position(startLine, startChar), new Position(endLine, endChar));
+  }
+
+  public static Range newRange(Token startToken, Token endToken) {
+    int startLine = startToken.getLine() - 1;
+    int startChar = startToken.getCharPositionInLine();
+    int endLine = endToken.getLine() - 1;
+    int endChar = endToken.getCharPositionInLine();
+
+    if (startToken.equals(endToken)) {
+      endChar += endToken.getText().length();
+    }
+
+    return newRange(startLine, startChar, endLine, endChar);
   }
 }
