37c9a6c405dac63619f3e69390f46a40862bd1be
Nikita Gryzlov <nixel2007@gmail.com>
2018-11-25 11:51:43 +0700

    Fix QF

--- src/main/java/org/github/_1c_syntax/intellij/bsl/lsp/server/FileInfo.java
+++ src/main/java/org/github/_1c_syntax/intellij/bsl/lsp/server/FileInfo.java
@@ -3,19 +3,20 @@ package org.github._1c_syntax.intellij.bsl.lsp.server;
 import org.antlr.v4.runtime.Token;
 import org.github._1c_syntax.parser.BSLParser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileInfo {
   private final BSLParser.FileContext tree;
   private final List<Token> tokens;
 
-  public FileInfo(BSLParser.FileContext tree, List<Token> tokens) {
+  FileInfo(BSLParser.FileContext tree, List<Token> tokens) {
     this.tree = tree;
-    this.tokens = tokens;
+    this.tokens = new ArrayList<>(tokens);
   }
 
   public List<Token> getTokens() {
-    return tokens;
+    return new ArrayList<>(tokens);
   }
 
   public BSLParser.FileContext getTree() {
