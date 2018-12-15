b644229abed1a7f2fe7d0cdec8157b44755f41c0
Nikita Gryzlov <nixel2007@gmail.com>
2018-12-15 13:54:55 +0700

    Общая реализация проверки диагностики

--- src/test/java/org/github/_1c_syntax/intellij/bsl/lsp/server/diagnostics/FunctionShouldHaveReturnDiagnosticTest.java
+++ src/test/java/org/github/_1c_syntax/intellij/bsl/lsp/server/diagnostics/FunctionShouldHaveReturnDiagnosticTest.java
@@ -21,45 +21,28 @@
  */
 package org.github._1c_syntax.intellij.bsl.lsp.server.diagnostics;
 
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.apache.commons.io.IOUtils;
 import org.eclipse.lsp4j.Diagnostic;
 import org.github._1c_syntax.intellij.bsl.lsp.server.utils.RangeHelper;
-import org.github._1c_syntax.parser.BSLLexer;
-import org.github._1c_syntax.parser.BSLParser;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 
-class FunctionShouldHaveReturnDiagnosticTest {
+class FunctionShouldHaveReturnDiagnosticTest extends AbstractDiagnosticTest<FunctionShouldHaveReturnDiagnostic>{
 
-  private BSLDiagnostic diagnostic = new FunctionShouldHaveReturnDiagnostic();
+  FunctionShouldHaveReturnDiagnosticTest() {
+    super(FunctionShouldHaveReturnDiagnostic.class);
+  }
 
   @Test
-  void getDiagnostics() throws IOException {
-
-    String textDocumentContent = IOUtils.resourceToString(
-      "diagnostics/FunctionShouldHaveReturnDiagnostic.bsl",
-      Charset.forName("UTF-8"),
-      this.getClass().getClassLoader()
-    );
-    BSLLexer lexer = new BSLLexer(CharStreams.fromString(textDocumentContent));
-
-    CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-    BSLParser parser = new BSLParser(tokens);
-
-    List<Diagnostic> diagnostics = diagnostic.getDiagnostics(parser.file());
+  void test() throws IOException {
+    List<Diagnostic> diagnostics = getDiagnostics();
 
     assertThat(diagnostics, hasSize(1));
     assertThat(diagnostics.get(0).getRange(), equalTo(RangeHelper.newRange(0, 8, 0, 26)));
-
   }
 }
