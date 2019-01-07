14ba4248e99f487be9231f55a819c31459350407
Nikita Gryzlov <nixel2007@gmail.com>
2019-01-07 14:31:58 +0300

    Smoke test на analyze

---
+++ src/test/java/org/github/_1c_syntax/intellij/bsl/lsp/server/cli/AnalyzeCommandTest.java
@@ -0,0 +1,28 @@
+package org.github._1c_syntax.intellij.bsl.lsp.server.cli;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.github._1c_syntax.intellij.bsl.lsp.server.BSLLSPLauncher.createOptions;
+
+class AnalyzeCommandTest {
+
+    @Test
+    void testExecute() throws ParseException {
+        Options options = createOptions();
+
+        DefaultParser parser = new DefaultParser();
+        CommandLine commandLine = parser.parse(options, new String[]{"-s", "/tmp"});
+
+        Command command = new AnalyzeCommand(commandLine);
+
+        int result = command.execute();
+
+        assertThat(result).isEqualTo(0);
+    }
+
+}
