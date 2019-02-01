9a06dade861f67cb448fbceaa86d057f13ec8426
Nikita Gryzlov <nixel2007@gmail.com>
2019-02-01 10:53:26 +0300

    #24 Репортер в JUnit.xml

---
+++ src/main/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/reporter/JUnitReporter.java
@@ -0,0 +1,55 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2019
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package org.github._1c_syntax.bsl.languageserver.diagnostics.reporter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JUnitReporter implements DiagnosticReporter {
+
+  public static final String KEY = "junit";
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(JUnitReporter.class.getSimpleName());
+
+  @Override
+  public void report(AnalysisInfo analysisInfo) {
+
+    JUnitTestSuites jUnitReport = new JUnitTestSuites(analysisInfo);
+
+    ObjectMapper mapper = new XmlMapper();
+    mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+    try {
+      File reportFile = new File("./bsl-junit.xml");
+      mapper.writeValue(reportFile, jUnitReport);
+      LOGGER.info("JUnit report saved to {}", reportFile.getAbsolutePath());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
---
+++ src/main/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/reporter/JUnitTestSuites.java
@@ -0,0 +1,165 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2019
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package org.github._1c_syntax.bsl.languageserver.diagnostics.reporter;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.dataformat.xml.annotation.*;
+import lombok.Data;
+import lombok.Getter;
+import org.eclipse.lsp4j.Diagnostic;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.FileInfo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@JacksonXmlRootElement(localName = "testsuites")
+class JUnitTestSuites {
+
+  @Getter
+  @JsonProperty("package")
+  @JacksonXmlProperty(isAttribute = true)
+  private final String packageName;
+
+  @Getter
+  @JacksonXmlElementWrapper(useWrapping = false)
+  private final List<JUnitTestSuite> testsuite;
+
+  public JUnitTestSuites(AnalysisInfo analysisInfo) {
+    packageName = "bsl-language-server";
+    testsuite = new ArrayList<>();
+
+    List<FileInfo> fileinfos = analysisInfo.getFileinfos();
+    fileinfos.stream()
+      .filter(fileInfo -> !fileInfo.getDiagnostics().isEmpty())
+      .forEach(fileInfo -> testsuite.add(new JUnitTestSuite(fileInfo)));
+  }
+
+  public JUnitTestSuites(
+    @JsonProperty("package") String packageName,
+    @JsonProperty("testsuite") List<JUnitTestSuite> testsuite
+  ) {
+    this.packageName = packageName;
+    this.testsuite = testsuite;
+  }
+
+  @Data
+  static class JUnitTestSuite {
+    @JacksonXmlProperty(isAttribute = true)
+    private final String name;
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private final List<JUnitTestCase> testcase;
+
+    public JUnitTestSuite(FileInfo fileInfo) {
+      this.name = fileInfo.getPath().toString();
+      this.testcase = new ArrayList<>();
+
+      List<Diagnostic> diagnostics = fileInfo.getDiagnostics();
+      diagnostics.forEach(diagnostic -> testcase.add(new JUnitTestCase(diagnostic, this.name))
+      );
+    }
+
+    public JUnitTestSuite(
+      @JsonProperty("name") String  name,
+      @JsonProperty("testcase") List<JUnitTestCase> testcase
+    ) {
+      this.name = name;
+      this.testcase = testcase;
+    }
+  }
+
+  @Data
+  static class JUnitTestCase {
+    @JacksonXmlProperty(isAttribute = true)
+    private final String name;
+
+    @JacksonXmlProperty(isAttribute = true)
+    private final String classname;
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private final List<JUnitFailure> failure;
+
+    public JUnitTestCase(Diagnostic diagnostic, String classname) {
+      this.name = diagnostic.getCode();
+      this.classname = classname;
+      this.failure = new ArrayList<>();
+
+      String type = diagnostic.getSeverity().toString().toLowerCase(Locale.ENGLISH);
+      String message = diagnostic.getMessage();
+      String value = String.format(
+        "line: %d, column: %d, text: %s",
+        diagnostic.getRange().getStart().getLine() + 1,
+        diagnostic.getRange().getStart().getCharacter(),
+        diagnostic.getMessage()
+      );
+
+      this.failure.add(new JUnitFailure(type, message, value));
+    }
+
+    public JUnitTestCase(
+      @JsonProperty("name") String  name,
+      @JsonProperty("classname") String classname,
+      @JsonProperty("failure") List<JUnitFailure> failure
+    ) {
+      this.name = name;
+      this.classname = classname;
+      this.failure = failure;
+    }
+  }
+
+  @Data
+  @JsonDeserialize(using = JUnitFailureDeserializer.class)
+  static class JUnitFailure {
+    @JacksonXmlProperty(isAttribute = true)
+    private final String type;
+
+    @JacksonXmlProperty(isAttribute = true)
+    private final String message;
+
+    @JacksonXmlText
+    @JacksonXmlCData
+    private final String value;
+  }
+
+  static class JUnitFailureDeserializer extends JsonDeserializer<JUnitFailure> {
+
+    @Override
+    public JUnitFailure deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+      JsonNode node = jp.getCodec().readTree(jp);
+
+      String type = node.get("type").asText("");
+      String message = node.get("message").asText("");
+      String value = node.get("").asText("");
+
+      return new JUnitFailure(type, message, value);
+
+    }
+  }
+
+}
