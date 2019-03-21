835108e9eb0cfd32c7001ecaa4ddec77d7e536d1
Nikita Gryzlov <nixel2007@gmail.com>
2019-03-21 22:38:25 +0300

    Явное указание версии java для компиляции

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,6 +58,11 @@ dependencies {
     testImplementation("com.ginsberg", "junit5-system-exit", "1.0.0")
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     options.compilerArgs.add("-Xlint:unchecked")
