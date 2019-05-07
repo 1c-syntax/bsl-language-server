154807f13b55409e78aad8c23ffbf12faa4de9c4
Leon Chagelishvili <lchagelishvily@gmail.com>
2019-05-08 00:27:49 +0300

    WIP: #97 Заготовка под диагностику

---
+++ src/main/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/NestedFunctionsInStructureDeclarationDiagnostic.java
@@ -0,0 +1,60 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2019
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+
+package org.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticScope;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
+import org.github._1c_syntax.bsl.languageserver.utils.DiagnosticHelper;
+import org.github._1c_syntax.bsl.parser.BSLParser;
+
+
+/**
+ * @author Leon Chagelishvili <lChagelishvily@gmail.com>
+ */
+@DiagnosticMetadata(
+  type = DiagnosticType.CODE_SMELL,
+  severity = DiagnosticSeverity.MINOR,
+  scope = DiagnosticScope.ALL,
+  minutesToFix = 10
+)
+
+public class NestedFunctionsInStructureDeclarationDiagnostic extends AbstractVisitorDiagnostic {
+
+  @Override
+  public ParseTree visitNewExpression(BSLParser.NewExpressionContext ctx) {
+
+    if (ctx.typeName() == null){
+      return ctx;
+    }
+
+    // TODO: Учесть, что структура может быть обявлена как "Новый ("Структура", МассивПараметров())"
+    if(!(DiagnosticHelper.isStructureType(ctx.typeName()) || DiagnosticHelper.isFixedStructureType(ctx.typeName()))){
+      return ctx;
+    }
+
+    return super.visitNewExpression(ctx);
+  }
+}
