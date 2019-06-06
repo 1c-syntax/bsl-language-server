7eb9c1fccdf8ab3c0570d0d28ca87ba54a0d4a6e
Nikita Gryzlov <nixel2007@gmail.com>
2019-06-06 15:43:55 +0300

    Добавление переменных в documentSymbol

--- src/main/java/org/github/_1c_syntax/bsl/languageserver/providers/DocumentSymbolProvider.java
+++ src/main/java/org/github/_1c_syntax/bsl/languageserver/providers/DocumentSymbolProvider.java
@@ -22,7 +22,6 @@
 package org.github._1c_syntax.bsl.languageserver.providers;
 
 import org.eclipse.lsp4j.DocumentSymbol;
-import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.SymbolInformation;
 import org.eclipse.lsp4j.SymbolKind;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
@@ -32,7 +31,9 @@ import org.github._1c_syntax.bsl.languageserver.utils.RangeHelper;
 import org.github._1c_syntax.bsl.parser.BSLParser;
 import org.github._1c_syntax.bsl.parser.BSLParserRuleContext;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -44,31 +45,86 @@ public final class DocumentSymbolProvider {
 
   public static List<Either<SymbolInformation, DocumentSymbol>> getDocumentSymbol(DocumentContext documentContext) {
 
-    List<DocumentSymbol> symbols = new ArrayList<>();
+    List<DocumentSymbol> globalVariables = getGlobalVariables(documentContext);
+
+    List<DocumentSymbol> symbols = new ArrayList<>(globalVariables);
+
     documentContext.getMethods().forEach((MethodSymbol methodSymbol) -> {
       BSLParserRuleContext context = methodSymbol.getNode();
-      Range range = RangeHelper.newRange(context);
-
-      BSLParser.SubNameContext subNameContext;
-      if (methodSymbol.isFunction()) {
-        subNameContext = ((BSLParser.FunctionContext) context).funcDeclaration().subName();
-      } else {
-        subNameContext = ((BSLParser.ProcedureContext) context).procDeclaration().subName();
-      }
-      Range selectionRange = RangeHelper.newRange(subNameContext);
+      BSLParser.SubNameContext subNameContext = getSubNameContext(methodSymbol);
 
       DocumentSymbol documentSymbol = new DocumentSymbol(
         methodSymbol.getName(),
         SymbolKind.Method,
-        range,
-        selectionRange
+        RangeHelper.newRange(context),
+        RangeHelper.newRange(subNameContext)
       );
-      
+
+      BSLParser.SubVarsContext subVariablesContext = getSubVarsContext(methodSymbol);
+      List<DocumentSymbol> subVariables = getSubVariables(subVariablesContext);
+
+      documentSymbol.setChildren(subVariables);
+
       symbols.add(documentSymbol);
     });
 
     return symbols.stream()
       .map(Either::<SymbolInformation, DocumentSymbol>forRight)
+      .collect(Collectors.toList());
+  }
+
+  private static BSLParser.SubNameContext getSubNameContext(MethodSymbol methodSymbol) {
+    BSLParser.SubNameContext subNameContext;
+    if (methodSymbol.isFunction()) {
+      subNameContext = ((BSLParser.FunctionContext) methodSymbol.getNode()).funcDeclaration().subName();
+    } else {
+      subNameContext = ((BSLParser.ProcedureContext) methodSymbol.getNode()).procDeclaration().subName();
+    }
+    return subNameContext;
+  }
+
+  private static BSLParser.SubVarsContext getSubVarsContext(MethodSymbol methodSymbol) {
+    BSLParser.SubVarsContext subVariablesContext;
+    if (methodSymbol.isFunction()) {
+      subVariablesContext = ((BSLParser.FunctionContext) methodSymbol.getNode()).subCodeBlock().subVars();
+    } else {
+      subVariablesContext = ((BSLParser.ProcedureContext) methodSymbol.getNode()).subCodeBlock().subVars();
+    }
+    return subVariablesContext;
+  }
+
+  private static List<DocumentSymbol> getSubVariables(@Nullable BSLParser.SubVarsContext subVariablesContext) {
+
+    if (subVariablesContext == null) {
+      return Collections.emptyList();
+    }
+
+    return subVariablesContext.subVar().stream()
+      .flatMap(subVarContext -> subVarContext.subVarsList().subVarDeclaration().stream())
+      .map(subVarDeclarationContext -> new DocumentSymbol(
+        subVarDeclarationContext.var_name().getText(),
+        SymbolKind.Variable,
+        RangeHelper.newRange(subVarDeclarationContext),
+        RangeHelper.newRange(subVarDeclarationContext.var_name())
+      ))
+      .collect(Collectors.toList());
+  }
+
+  private static List<DocumentSymbol> getGlobalVariables(DocumentContext documentContext) {
+    BSLParser.ModuleVarsContext moduleVarsContext = documentContext.getAst().moduleVars();
+
+    if (moduleVarsContext == null) {
+      return Collections.emptyList();
+    }
+
+    return moduleVarsContext.moduleVar().stream()
+      .flatMap(moduleVarContext -> moduleVarContext.moduleVarsList().moduleVarDeclaration().stream())
+      .map(moduleVarDeclarationContext -> new DocumentSymbol(
+        moduleVarDeclarationContext.var_name().getText(),
+        SymbolKind.Variable,
+        RangeHelper.newRange(moduleVarDeclarationContext),
+        RangeHelper.newRange(moduleVarDeclarationContext.var_name())
+      ))
       .collect(Collectors.toList());
   }
 }
