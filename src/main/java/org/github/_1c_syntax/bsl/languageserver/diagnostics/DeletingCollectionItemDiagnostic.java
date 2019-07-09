6d00ea85d07296f4133f7e746acb7b1f59c5ef17
Alexander Strizhachuk <strizhhh@mail.ru>
2019-07-09 12:20:44 +0300

    Реализация диагностики DeletingCollectionItem.

--- src/main/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/DeletingCollectionItemDiagnostic.java
+++ src/main/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/DeletingCollectionItemDiagnostic.java
@@ -1,3 +1,24 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2019
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
 package org.github._1c_syntax.bsl.languageserver.diagnostics;
 
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -6,6 +27,9 @@ import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticM
 import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
 import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
 import org.github._1c_syntax.bsl.parser.BSLParser;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @DiagnosticMetadata(
   type = DiagnosticType.ERROR,
@@ -16,9 +40,26 @@ public class DeletingCollectionItemDiagnostic extends AbstractVisitorDiagnostic 
 
   @Override
   public ParseTree visitForEachStatement(BSLParser.ForEachStatementContext ctx) {
-    diagnosticStorage.addDiagnostic(ctx);
+
+    String iterator = ctx.IDENTIFIER().getText();
+    String expression = ctx.expression().getText();
+
+    List<ParseTree> childIdentifiers = Trees.findAllTokenNodes(ctx.codeBlock(), BSLParser.IDENTIFIER)
+      .stream()
+      .filter( node -> node.getParent().getClass() == BSLParser.MethodNameContext.class
+        && node.getParent().getParent().getClass() != BSLParser.GlobalMethodCallContext.class )
+      .filter( node -> node.getText().equalsIgnoreCase("удалить")
+        || node.getText().equalsIgnoreCase("delete") )
+      .filter( node -> node.getParent().getParent().getParent().getParent().getText().startsWith(
+          expression + "." + node.getParent().getText() ) )
+      .filter( node -> node.getParent().getParent().getText().contains( iterator ) )
+      .collect(Collectors.toList());
+
+    if (!childIdentifiers.isEmpty()) {
+      diagnosticStorage.addDiagnostic(ctx, expression);
+    }
+
     return super.visitForEachStatement(ctx);
+
   }
-
-
 }
--- src/test/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/DeletingCollectionItemDiagnosticTest.java
+++ src/test/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/DeletingCollectionItemDiagnosticTest.java
@@ -1,6 +1,28 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2019
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
 package org.github._1c_syntax.bsl.languageserver.diagnostics;
 
 import org.eclipse.lsp4j.Diagnostic;
+import org.github._1c_syntax.bsl.languageserver.utils.RangeHelper;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -16,7 +38,14 @@ public class DeletingCollectionItemDiagnosticTest extends AbstractDiagnosticTest
   @Test
   void test() {
     List<Diagnostic> diagnostics = getDiagnostics();
-    assertThat(diagnostics).hasSize(1);
+    assertThat(diagnostics).hasSize(6);
+    assertThat(diagnostics.get(0).getRange()).isEqualTo(RangeHelper.newRange(15, 0, 19, 10));
+    assertThat(diagnostics.get(1).getRange()).isEqualTo(RangeHelper.newRange(22, 0, 24, 5));
+    assertThat(diagnostics.get(2).getRange()).isEqualTo(RangeHelper.newRange(27, 0, 29, 5));
+    assertThat(diagnostics.get(3).getRange()).isEqualTo(RangeHelper.newRange(32, 0, 34, 10));
+    assertThat(diagnostics.get(4).getRange()).isEqualTo(RangeHelper.newRange(37, 0, 41, 10));
+    assertThat(diagnostics.get(5).getRange()).isEqualTo(RangeHelper.newRange(44, 0, 46, 5));
+
   }
 
 }
