2ddb261f09489baf7aec5b92c5b60f25d3869139
Сергей Яныгин <yanyginsa@gmail.com>
2019-08-12 17:42:00 +0300

    Добавление диагностики Пропущенные пробелы (набросок)

---
+++ src/test/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/MissingSpaceDiagnosticTest.java
@@ -0,0 +1,52 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2019
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package org.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import org.eclipse.lsp4j.Diagnostic;
+import org.github._1c_syntax.bsl.languageserver.providers.DiagnosticProvider;
+import org.github._1c_syntax.bsl.languageserver.utils.RangeHelper;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MissingSpaceDiagnosticTest extends AbstractDiagnosticTest<MissingSpaceDiagnostic> {
+
+  MissingSpaceDiagnosticTest() {
+    super(MissingSpaceDiagnostic.class);
+  }
+
+  @Test
+  void test() {
+    List<Diagnostic> diagnostics = getDiagnostics();
+
+    assertThat(diagnostics).hasSize(3);
+    assertThat(diagnostics)
+      .anyMatch(diagnostic -> diagnostic.getRange().equals(RangeHelper.newRange(0, 15, 0, 15)))
+      .anyMatch(diagnostic -> diagnostic.getRange().equals(RangeHelper.newRange(2, 2, 2, 2)))
+      .anyMatch(diagnostic -> diagnostic.getRange().equals(RangeHelper.newRange(2, 4, 2, 4)));
+  }
+
+
+}
