4746b5795ec4aa445eb344a9b7ff7d7a4d75addf
Dmitry <dajigin@mail.ru>
2019-09-08 14:25:20 +0300

    #342 After Optimizing imports some project files are modified

--- src/main/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/PairingBrokenTransactionDiagnostic.java
+++ src/main/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/PairingBrokenTransactionDiagnostic.java
@@ -21,19 +21,19 @@
  */
 package org.github._1c_syntax.bsl.languageserver.diagnostics;
 
-  import org.antlr.v4.runtime.tree.ParseTree;
-  import org.antlr.v4.runtime.tree.Trees;
-  import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
-  import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
-  import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
-  import org.github._1c_syntax.bsl.parser.BSLParser;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.Trees;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
+import org.github._1c_syntax.bsl.parser.BSLParser;
 
-  import java.util.ArrayDeque;
-  import java.util.ArrayList;
-  import java.util.HashMap;
-  import java.util.Locale;
-  import java.util.regex.Pattern;
-  import java.util.stream.Collectors;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @DiagnosticMetadata(
   type = DiagnosticType.ERROR,
