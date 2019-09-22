2e3aa1b7f26ce4ba69570ac769797b2f66b0a850
Tymko Oleg <olegtymko@yandex.ru>
2019-09-22 23:19:39 +0700

    Диагностика: запрещено хранить конф. информацию в коде

---
+++ src/main/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/UsingHardcodeSecretInformationDiagnostic.java
@@ -0,0 +1,160 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2019
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package org.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticParameter;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticScope;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
+import org.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
+import org.github._1c_syntax.bsl.parser.BSLParser;
+import org.github._1c_syntax.bsl.parser.BSLParserRuleContext;
+
+import javax.annotation.CheckForNull;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@DiagnosticMetadata(
+  type = DiagnosticType.VULNERABILITY,
+  severity = DiagnosticSeverity.CRITICAL,
+  scope = DiagnosticScope.BSL,
+  minutesToFix = 15
+)
+public class UsingHardcodeSecretInformationDiagnostic extends AbstractVisitorDiagnostic {
+
+  private static final String FIND_WORD_DEFAULT = "Пароль|Password";
+
+  private final Pattern patternNewExpression = Pattern.compile(
+    "Структура|Structure|Соответствие|Map",
+    Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+
+  private final Pattern patternMethodInsert = Pattern.compile(
+    "Вставить|Insert",
+    Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+
+  private final String SEPARATOR = ",";
+
+  @DiagnosticParameter(
+    type = String.class,
+    defaultValue = "" + FIND_WORD_DEFAULT,
+    description = "Слова поиска конфидициальной информации"
+  )
+  private String searchWords = FIND_WORD_DEFAULT;
+
+  private Pattern pattern = getPatternSearch(FIND_WORD_DEFAULT);
+
+  @Override
+  public void configure(Map<String, Object> configuration) {
+    if (configuration == null) {
+      return;
+    }
+    searchWords = (String) configuration.get("searchWords");
+    pattern = getPatternSearch(searchWords);
+  }
+
+  private Pattern getPatternSearch(String value) {
+    return Pattern.compile(
+      "^(" + value + ")",
+      Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+  }
+
+  @Override
+  public ParseTree visitAssignment(BSLParser.AssignmentContext ctx) {
+    Matcher matcher = pattern.matcher(ctx.getStart().getText());
+    if (matcher.find() && isNotEmptyStringByToken(ctx.getStop())) {
+      diagnosticStorage.addDiagnostic(ctx, getDiagnosticMessage());
+    }
+    return super.visitAssignment(ctx);
+  }
+
+  @Override
+  public ParseTree visitMethodCall(BSLParser.MethodCallContext ctx) {
+    Matcher matcherMethod = patternMethodInsert.matcher(ctx.methodName().getText());
+    if (matcherMethod.find()) {
+      List<BSLParser.CallParamContext> list = ctx.doCall().callParamList().callParam();
+      Matcher matcher = pattern.matcher(getClearString(list.get(0).getText()));
+      if (matcher.find() && list.size() == 2) {
+        if (isNotEmptyStringByToken(list.get(1).getStart())) {
+          addDiagnosticByAssignment(ctx, BSLParser.RULE_statement);
+        }
+      }
+    }
+    return super.visitMethodCall(ctx);
+  }
+
+  @Override
+  public ParseTree visitNewExpression(BSLParser.NewExpressionContext ctx) {
+    Matcher matcherTypeName = patternNewExpression.matcher(ctx.typeName().getText());
+    if (matcherTypeName.find()) {
+      BSLParser.DoCallContext doCallContext = ctx.doCall();
+      if (doCallContext != null) {
+        List<BSLParser.CallParamContext> list = doCallContext.callParamList().callParam();
+        if (list.size() > 0) {
+          String[] arr = list.get(0).getText().split(SEPARATOR);
+          for (int index = 0; index < arr.length; index++) {
+            Matcher matcher = pattern.matcher(getClearString(arr[index]));
+            if (matcher.find() && list.size() >= index) {
+              if (isNotEmptyStringByToken(list.get(index + 1).getStart())) {
+                addDiagnosticByAssignment(ctx, BSLParser.RULE_assignment);
+              }
+            }
+          }
+        }
+      }
+    }
+    return super.visitNewExpression(ctx);
+  }
+
+  private void addDiagnosticByAssignment(BSLParserRuleContext ctx, int type) {
+    ParserRuleContext assignment = getAncestorByRuleIndex((ParserRuleContext) ctx.getRuleContext(), type);
+    if (assignment != null) {
+      diagnosticStorage.addDiagnostic((BSLParserRuleContext) assignment, getDiagnosticMessage());
+    }
+  }
+
+  private boolean isNotEmptyStringByToken(Token token) {
+    return token.getType() == BSLParser.STRING && !(token.getText().length() == 2);
+  }
+
+  private String getClearString(String inputString) {
+    return inputString.replaceAll("\"", "").replaceAll(" ", "");
+  }
+
+  // TODO: перенести в bsl parser
+  @CheckForNull
+  private ParserRuleContext getAncestorByRuleIndex(ParserRuleContext element, int type) {
+    ParserRuleContext parent = element.getParent();
+    if (parent == null) {
+      return null;
+    }
+    if (parent.getRuleIndex() == type) {
+      return parent;
+    }
+    return getAncestorByRuleIndex(parent, type);
+  }
+
+}
---
+++ src/test/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/UsingHardcodeSecretInformationDiagnosticTest.java
@@ -0,0 +1,55 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2019
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package org.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import org.eclipse.lsp4j.Diagnostic;
+import org.github._1c_syntax.bsl.languageserver.utils.RangeHelper;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UsingHardcodeSecretInformationDiagnosticTest extends AbstractDiagnosticTest<UsingHardcodeSecretInformationDiagnostic> {
+	UsingHardcodeSecretInformationDiagnosticTest() {
+		super(UsingHardcodeSecretInformationDiagnostic.class);
+	}
+
+	@Test
+	public void test() {
+
+		// when
+		List<Diagnostic> diagnostics = getDiagnostics();
+
+		// then
+		assertThat(diagnostics).hasSize(5);
+
+		assertThat(diagnostics)
+			.anyMatch(diagnostic -> diagnostic.getRange().equals(RangeHelper.newRange(8, 4, 8, 49)))
+			.anyMatch(diagnostic -> diagnostic.getRange().equals(RangeHelper.newRange(12, 4, 12, 80)))
+			.anyMatch(diagnostic -> diagnostic.getRange().equals(RangeHelper.newRange(16, 4, 16, 23)))
+			.anyMatch(diagnostic -> diagnostic.getRange().equals(RangeHelper.newRange(17, 4, 17, 23)))
+			.anyMatch(diagnostic -> diagnostic.getRange().equals(RangeHelper.newRange(27, 4, 27, 35)))
+		;
+
+	}
+}
