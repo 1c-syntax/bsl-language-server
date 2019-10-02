fcd43c67001350afff035a7cba9b6e493173686f
Nikita Gryzlov <nixel2007@gmail.com>
2019-10-02 13:18:38 +0300

    reformat

--- src/main/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/UnreachableCodeDiagnostic.java
+++ src/main/java/org/github/_1c_syntax/bsl/languageserver/diagnostics/UnreachableCodeDiagnostic.java
@@ -38,95 +38,95 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @DiagnosticMetadata(
-	type = DiagnosticType.ERROR,
-	severity = DiagnosticSeverity.MINOR,
-	minutesToFix = 10
+  type = DiagnosticType.ERROR,
+  severity = DiagnosticSeverity.MINOR,
+  minutesToFix = 10
 )
 public class UnreachableCodeDiagnostic extends AbstractVisitorDiagnostic {
 
-	// кэш диапазонов зарегистрированных ошибок
-	private List<Range> ranges = new ArrayList<>();
+  // кэш диапазонов зарегистрированных ошибок
+  private List<Range> ranges = new ArrayList<>();
 
-	@Override
-	public ParseTree visitFile(BSLParser.FileContext ctx) {
-		ranges.clear();
-		return super.visitFile(ctx);
-	}
+  @Override
+  public ParseTree visitFile(BSLParser.FileContext ctx) {
+    ranges.clear();
+    return super.visitFile(ctx);
+  }
 
-	@Override
-	public ParseTree visitContinueStatement(BSLParser.ContinueStatementContext ctx) {
-		findAndAddDiagnostic(ctx);
-		return super.visitContinueStatement(ctx);
-	}
+  @Override
+  public ParseTree visitContinueStatement(BSLParser.ContinueStatementContext ctx) {
+    findAndAddDiagnostic(ctx);
+    return super.visitContinueStatement(ctx);
+  }
 
-	@Override
-	public ParseTree visitReturnStatement(BSLParser.ReturnStatementContext ctx) {
-		findAndAddDiagnostic(ctx);
-		return super.visitReturnStatement(ctx);
-	}
+  @Override
+  public ParseTree visitReturnStatement(BSLParser.ReturnStatementContext ctx) {
+    findAndAddDiagnostic(ctx);
+    return super.visitReturnStatement(ctx);
+  }
 
-	@Override
-	public ParseTree visitGotoStatement(BSLParser.GotoStatementContext ctx) {
-		findAndAddDiagnostic(ctx);
-		return super.visitGotoStatement(ctx);
-	}
+  @Override
+  public ParseTree visitGotoStatement(BSLParser.GotoStatementContext ctx) {
+    findAndAddDiagnostic(ctx);
+    return super.visitGotoStatement(ctx);
+  }
 
-	@Override
-	public ParseTree visitRaiseStatement(BSLParser.RaiseStatementContext ctx) {
-		findAndAddDiagnostic(ctx);
-		return super.visitRaiseStatement(ctx);
-	}
+  @Override
+  public ParseTree visitRaiseStatement(BSLParser.RaiseStatementContext ctx) {
+    findAndAddDiagnostic(ctx);
+    return super.visitRaiseStatement(ctx);
+  }
 
-	@Override
-	public ParseTree visitBreakStatement(BSLParser.BreakStatementContext ctx) {
-		findAndAddDiagnostic(ctx);
-		return super.visitBreakStatement(ctx);
-	}
+  @Override
+  public ParseTree visitBreakStatement(BSLParser.BreakStatementContext ctx) {
+    findAndAddDiagnostic(ctx);
+    return super.visitBreakStatement(ctx);
+  }
 
-	private void findAndAddDiagnostic(BSLParserRuleContext ctx) {
+  private void findAndAddDiagnostic(BSLParserRuleContext ctx) {
 
-		// если это вложенный в ранее обработанный блок, то исключим из проверки
-		Position pos = new Position(ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine());
-		for (Range range : ranges) {
-			if (Ranges.containsPosition(range, pos)) {
-				return;
-			}
-		}
+    // если это вложенный в ранее обработанный блок, то исключим из проверки
+    Position pos = new Position(ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine());
+    for (Range range : ranges) {
+      if (Ranges.containsPosition(range, pos)) {
+        return;
+      }
+    }
 
-		BSLParserRuleContext nodeParent = (BSLParserRuleContext) ctx.getParent();
-		if(nodeParent == null) {
-			return;
-		}
+    BSLParserRuleContext nodeParent = (BSLParserRuleContext) ctx.getParent();
+    if (nodeParent == null) {
+      return;
+    }
 
-		BSLParser.StatementContext ppNode = (BSLParser.StatementContext) nodeParent.getParent();
-		if(ppNode == null) {
-			return;
-		}
+    BSLParser.StatementContext ppNode = (BSLParser.StatementContext) nodeParent.getParent();
+    if (ppNode == null) {
+      return;
+    }
 
-		BSLParserRuleContext ppNodeParent = (BSLParserRuleContext) ppNode.getParent();
-		if(ppNodeParent == null) {
-			return;
-		}
+    BSLParserRuleContext ppNodeParent = (BSLParserRuleContext) ppNode.getParent();
+    if (ppNodeParent == null) {
+      return;
+    }
 
-		List<ParseTree> statements = Trees.findAllRuleNodes(ppNodeParent, BSLParser.RULE_statement)
-			.stream()
-			.filter(node -> node.getParent().equals(ppNodeParent))
-			.collect(Collectors.toList());
+    List<ParseTree> statements = Trees.findAllRuleNodes(ppNodeParent, BSLParser.RULE_statement)
+      .stream()
+      .filter(node -> node.getParent().equals(ppNodeParent))
+      .collect(Collectors.toList());
 
-		// если в блоке кода есть еще стейты кроме текущего
-		if(statements.size() > 1) {
-			BSLParserRuleContext endCurrentBlockNode = (BSLParserRuleContext) statements.get(statements.size() - 1);
+    // если в блоке кода есть еще стейты кроме текущего
+    if (statements.size() > 1) {
+      BSLParserRuleContext endCurrentBlockNode = (BSLParserRuleContext) statements.get(statements.size() - 1);
 
-			// если последний стей не текущий, значит он будет недостижим
-			if(!ppNode.equals(endCurrentBlockNode)) {
-				int indexStart = statements.indexOf(ppNode);
-				Range newRange = RangeHelper.newRange(
-					((BSLParserRuleContext) statements.get(indexStart + 1)).getStart(),
-					endCurrentBlockNode.getStop());
-				diagnosticStorage.addDiagnostic(newRange);
-				// сохраним в кэш
-				ranges.add(newRange);
-			}
-		}
-	}
+      // если последний стей не текущий, значит он будет недостижим
+      if (!ppNode.equals(endCurrentBlockNode)) {
+        int indexStart = statements.indexOf(ppNode);
+        Range newRange = RangeHelper.newRange(
+          ((BSLParserRuleContext) statements.get(indexStart + 1)).getStart(),
+          endCurrentBlockNode.getStop());
+        diagnosticStorage.addDiagnostic(newRange);
+        // сохраним в кэш
+        ranges.add(newRange);
+      }
+    }
+  }
 }
