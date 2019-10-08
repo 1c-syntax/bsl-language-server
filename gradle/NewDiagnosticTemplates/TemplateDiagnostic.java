5a76a84898aba7f0d15d207ff8cadb3cc660116d
Maximov Valery <maximovvalery@gmail.com>
2019-10-08 22:32:42 +0300

    Добавил интерактивное указание тэгов для диагностики

--- gradle/NewDiagnosticTemplates/TemplateDiagnostic.java
+++ gradle/NewDiagnosticTemplates/TemplateDiagnostic.java
@@ -2,12 +2,14 @@ package com.github._1c_syntax.bsl.languageserver.diagnostics;
 
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticTags;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
 
 @DiagnosticMetadata(
   type = DiagnosticType.CODE_SMELL,
   severity = DiagnosticSeverity.INFO,
-  minutesToFix = 1
+  minutesToFix = 1,
+  $diagnosticTags
 )
 
 public class TemplateDiagnostic extends AbstractVisitorDiagnostic {
