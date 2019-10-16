91c79f320dff4174d4af24e744fab517666ff7d2
ovsiankin.aa@gmail.com <ovsiankin.aa@gmail.com>
2019-10-16 22:20:26 +0300

    Лицензии и дкументация диагностик

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/NonExportMethodsInApiRegionDiagnostic.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/NonExportMethodsInApiRegionDiagnostic.java
@@ -1,3 +1,24 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2019
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
 package com.github._1c_syntax.bsl.languageserver.diagnostics;
 
 import com.github._1c_syntax.bsl.languageserver.context.symbol.MethodSymbol;
@@ -19,7 +40,7 @@ import java.util.regex.Pattern;
 public class NonExportMethodsInApiRegionDiagnostic extends AbstractVisitorDiagnostic {
 
   private static final Pattern REGION_NAME = Pattern.compile(
-    "^(?:ПрограммныйИнтерфейс|СлужебныйПрограмныйИнтерфейс|Public|Internal)$",
+    "^(?:Программный�?нтерфейс|СлужебныйПрограмный�?нтерфейс|Public|Internal)$",
     Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE
   );
 
