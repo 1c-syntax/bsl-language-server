2556be64b1de504845e4106ca872dd0f2bc94c90
boris stoyanov <info@stoyanov-boris.ru>
2019-10-18 18:48:49 +0500

    Реагируем на "++"

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/UnaryPlusInConcatenationDiagnostic.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/UnaryPlusInConcatenationDiagnostic.java
@@ -4,11 +4,14 @@ import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticM
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
 import com.github._1c_syntax.bsl.parser.BSLParser;
+import com.github._1c_syntax.bsl.parser.BSLParserRuleContext;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 @DiagnosticMetadata(
     type = DiagnosticType.ERROR,
@@ -18,12 +21,16 @@ import java.util.regex.Pattern;
 public class UnaryPlusInConcatenationDiagnostic extends AbstractVisitorDiagnostic {
 
   @Override
-  public ParseTree visitStatement(BSLParser.StatementContext ctx) {
-    // ctx.children.stream().forEach()
-    return super.visitStatement(ctx);
+  public ParseTree visitExpression(BSLParser.ExpressionContext ctx) {
+    boolean isError = ctx.children.size() == 3
+      && ctx.children.get(1).getChildCount() == 1
+      && ctx.children.get(1).getChild(0).toString().equals("+")
+      && ctx.children.get(2).getChildCount() == 2
+      && ctx.children.get(2).getChild(0).getChild(0).toString().equals("+");
+    if (isError) {
+      diagnosticStorage.addDiagnostic((TerminalNode) ctx.children.get(1).getChild(0));
+    }
+    return super.visitExpression(ctx);
   }
 
-  private static void addErrorsRecursively() {
-
-  }
 }
