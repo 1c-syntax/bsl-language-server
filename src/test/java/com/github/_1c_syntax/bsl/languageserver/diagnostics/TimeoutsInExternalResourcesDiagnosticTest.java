abba40588b00f184900490566e2ce04b1f7d0f3b
Alexey Sosnoviy <int-it@yandex.ru>
2019-11-18 20:48:19 +0300

    А не надо в последний момент править код

--- src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/TimeoutsInExternalResourcesDiagnosticTest.java
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/TimeoutsInExternalResourcesDiagnosticTest.java
@@ -52,7 +52,7 @@ class TimeoutsInExternalResourcesDiagnosticTest extends AbstractDiagnosticTest<T
       .hasRange(34, 14, 34, 43)
       .hasRange(71, 26, 71, 114)
       .hasRange(78, 10, 78, 39)
-      .hasRange(81, 47, 81, 76)
+      .hasRange(80, 47, 80, 76)
     ;
 
   }
