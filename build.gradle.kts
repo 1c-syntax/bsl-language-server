6d961c8a6fe2d451aefc1fe124b2d2aab0bb0616
Alexey Sosnoviy <int-it@yandex.ru>
2019-11-23 23:16:04 +0300

    CompilationDirectiveLostDiagnostic #395

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     testRuntime("org.junit.jupiter", "junit-jupiter-engine", "5.5.2")
 
     testCompile("org.assertj", "assertj-core", "3.13.2")
+    testCompile("org.mockito", "mockito-core", "3.1.0")
 
     testImplementation("com.ginsberg", "junit5-system-exit", "1.0.0")
 }
