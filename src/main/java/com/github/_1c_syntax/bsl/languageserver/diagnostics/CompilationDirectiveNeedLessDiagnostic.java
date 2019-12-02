ce4dcd8df1b6520e87ca2acc562f5f94f7fc9696
Alexey Sosnoviy <int-it@yandex.ru>
2019-12-02 22:39:58 +0300

    NeedLessCompilationDirectiveDiagnostic #418

---
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/CompilationDirectiveNeedLessDiagnostic.java
@@ -0,0 +1,66 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2019
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package com.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticInfo;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticScope;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticTag;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
+import com.github._1c_syntax.bsl.parser.BSLParser;
+import com.github._1c_syntax.mdclasses.metadata.additional.ModuleType;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+@DiagnosticMetadata(
+  type = DiagnosticType.CODE_SMELL,
+  severity = DiagnosticSeverity.MAJOR,
+  scope = DiagnosticScope.BSL,
+  modules = {
+    ModuleType.ApplicationModule,
+    ModuleType.CommonModule,
+    ModuleType.ExternalConnectionModule,
+    ModuleType.ManagedApplicationModule,
+    ModuleType.ManagerModule,
+    ModuleType.ObjectModule,
+    ModuleType.OrdinaryApplicationModule,
+    ModuleType.RecordSetModule,
+    ModuleType.SessionModule,
+    ModuleType.ValueManagerModule
+  },
+  minutesToFix = 1,
+  tags = {
+    DiagnosticTag.CLUMSY
+  }
+
+)
+public class CompilationDirectiveNeedLessDiagnostic extends AbstractVisitorDiagnostic {
+  public CompilationDirectiveNeedLessDiagnostic(DiagnosticInfo info) {
+    super(info);
+  }
+
+  @Override
+  public ParseTree visitCompilerDirective(BSLParser.CompilerDirectiveContext ctx) {
+    diagnosticStorage.addDiagnostic(ctx);
+    return ctx;
+  }
+}
