4bd40bbec38c5e43e7ce46f71d812b52a87752c6
Nikita Gryzlov <nixel2007@gmail.com>
2019-12-03 18:35:56 +0300

    Переезд на шестой градль и shadowJar плагин

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,18 @@
+
 import org.apache.tools.ant.filters.EscapeUnicode
 import java.net.URI
 import java.util.*
 
 plugins {
     java
-    maven
+    `maven-publish`
     jacoco
     id("com.github.hierynomus.license") version "0.15.0"
     id("org.sonarqube") version "2.8"
     id("io.franzbecker.gradle-lombok") version "3.2.0"
     id("com.github.gradle-git-version-calculator") version "1.1.0"
     id("com.github.ben-manes.versions") version "0.25.0"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 repositories {
@@ -23,38 +25,37 @@ version = gitVersionCalculator.calculateVersion("v")
 
 dependencies {
     // https://mvnrepository.com/artifact/org.eclipse.lsp4j/org.eclipse.lsp4j
-    compile("org.eclipse.lsp4j", "org.eclipse.lsp4j", "0.8.1")
+    implementation("org.eclipse.lsp4j", "org.eclipse.lsp4j", "0.8.1")
 
     // https://mvnrepository.com/artifact/commons-cli/commons-cli
-    compile("commons-cli", "commons-cli", "1.4")
+    implementation("commons-cli", "commons-cli", "1.4")
     // https://mvnrepository.com/artifact/commons-io/commons-io
-    compile("commons-io", "commons-io", "2.6")
-    compile("org.apache.commons", "commons-lang3", "3.9")
+    implementation("commons-io", "commons-io", "2.6")
+    implementation("org.apache.commons", "commons-lang3", "3.9")
     // https://mvnrepository.com/artifact/commons-beanutils/commons-beanutils
-    compile("commons-beanutils", "commons-beanutils", "1.9.4")
+    implementation("commons-beanutils", "commons-beanutils", "1.9.4")
 
-    compile("com.fasterxml.jackson.core", "jackson-databind", "2.10.0")
-    compile("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310", "2.10.0")
-    compile("com.fasterxml.jackson.dataformat", "jackson-dataformat-xml", "2.10.0")
+    implementation("com.fasterxml.jackson.core", "jackson-databind", "2.10.0")
+    implementation("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310", "2.10.0")
+    implementation("com.fasterxml.jackson.dataformat", "jackson-dataformat-xml", "2.10.0")
 
     // https://mvnrepository.com/artifact/javax.xml.bind/jaxb-api
-    compile("javax.xml.bind", "jaxb-api", "2.3.1")
-
+    implementation("javax.xml.bind", "jaxb-api", "2.3.1")
 
     // https://mvnrepository.com/artifact/com.google.code.findbugs/jsr305
-    compile("com.google.code.findbugs", "jsr305", "3.0.2")
+    implementation("com.google.code.findbugs", "jsr305", "3.0.2")
 
     // https://github.com/1c-syntax/bsl-language-server/issues/369
     // Excude jline and use fixed one.
-    compile("me.tongfei", "progressbar", "0.7.4") { exclude(group = "org.jline") }
-    compile("org.jline", "jline", "3.13.1")
+    implementation("me.tongfei", "progressbar", "0.7.4") { exclude(group = "org.jline") }
+    implementation("org.jline", "jline", "3.13.1")
 
-    compile("org.slf4j", "slf4j-api", "1.8.0-beta4")
-    compile("org.slf4j", "slf4j-simple", "1.8.0-beta4")
+    implementation("org.slf4j", "slf4j-api", "1.8.0-beta4")
+    implementation("org.slf4j", "slf4j-simple", "1.8.0-beta4")
 
-    compile("org.reflections", "reflections", "0.9.10")
+    implementation("org.reflections", "reflections", "0.9.10")
 
-    compile("com.github.1c-syntax", "bsl-parser", "0.11.0") {
+    implementation("com.github.1c-syntax", "bsl-parser", "0.11.0") {
         exclude("com.github.nixel2007.antlr4", "antlr4-maven-plugin")
         exclude("com.github.nixel2007.antlr4", "antlr4-runtime-test-annotations")
         exclude("com.github.nixel2007.antlr4", "antlr4-runtime-test-annotation-processors")
@@ -67,14 +68,14 @@ dependencies {
         exclude("org.glassfish", "javax.json")
     }
 
-    compile("com.github.1c-syntax:mdclasses:9df30a46ff35f99d8478e0634198e203badfbcfd")
+    implementation("com.github.1c-syntax:mdclasses:9df30a46ff35f99d8478e0634198e203badfbcfd")
 
     compileOnly("org.projectlombok", "lombok", lombok.version)
 
     testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.5.2")
-    testRuntime("org.junit.jupiter", "junit-jupiter-engine", "5.5.2")
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.5.2")
 
-    testCompile("org.assertj", "assertj-core", "3.13.2")
+    testImplementation("org.assertj", "assertj-core", "3.13.2")
 
     testImplementation("com.ginsberg", "junit5-system-exit", "1.0.0")
 }
@@ -90,19 +91,20 @@ tasks.withType<JavaCompile> {
     options.compilerArgs.add("-Xlint:deprecation")
 }
 
-tasks.withType<Jar> {
+tasks.jar {
     manifest {
         attributes["Main-Class"] = "com.github._1c_syntax.bsl.languageserver.BSLLSPLauncher"
         attributes["Implementation-Version"] = archiveVersion.get()
     }
-    configurations["compile"].forEach {
-        from(zipTree(it.absoluteFile)) {
-            exclude("META-INF/MANIFEST.MF")
-            exclude("META-INF/*.SF")
-            exclude("META-INF/*.DSA")
-            exclude("META-INF/*.RSA")
-        }
-    }
+
+    enabled = false
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.shadowJar {
+    project.configurations.implementation.get().isCanBeResolved = true
+    configurations = listOf(project.configurations["implementation"])
+    archiveClassifier.set("")
 }
 
 tasks.test {
