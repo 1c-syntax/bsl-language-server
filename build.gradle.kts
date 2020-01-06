ecdf2938cd0918b3679d9b3d72766df663afdeb5
Maxmov Valery <maximovvalery@gmail.com>
2020-01-06 09:58:54 +0300

    Откатил progressbar и бампнул только jline

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,7 +84,10 @@ dependencies {
     // https://mvnrepository.com/artifact/com.google.code.findbugs/jsr305
     implementation("com.google.code.findbugs", "jsr305", "3.0.2")
 
-    implementation("me.tongfei", "progressbar", "0.8.0")
+    // https://github.com/1c-syntax/bsl-language-server/issues/369
+    // Excude jline and use fixed one.
+    implementation("me.tongfei", "progressbar", "0.7.4") { exclude(group = "org.jline") }
+    implementation("org.jline", "jline", "3.13.2")
 
     implementation("org.slf4j", "slf4j-api", "1.8.0-beta4")
     implementation("org.slf4j", "slf4j-simple", "1.8.0-beta4")
