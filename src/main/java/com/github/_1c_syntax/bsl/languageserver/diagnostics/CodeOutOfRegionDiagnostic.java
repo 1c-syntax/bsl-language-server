47deaea50e78db638262bca3c86beb85c7e2846c
Alexey Sosnoviy <int-it@yandex.ru>
2020-01-15 23:39:44 +0300

    fix preproc fp OutOfRegion

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/CodeOutOfRegionDiagnostic.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/CodeOutOfRegionDiagnostic.java
@@ -121,8 +121,7 @@ public class CodeOutOfRegionDiagnostic extends AbstractVisitorDiagnostic {
     Trees.findAllRuleNodes(ctx, BSLParser.RULE_statement)
       .forEach((ParseTree child) -> {
         if (child.getParent() instanceof BSLParser.CodeBlockContext
-          && Trees.findAllRuleNodes(child, BSLParser.RULE_regionEnd).isEmpty()
-          && Trees.findAllRuleNodes(child, BSLParser.RULE_regionStart).isEmpty()) {
+          && Trees.findAllRuleNodes(child, BSLParser.RULE_preprocessor).isEmpty()) {
 
           Range ctxRange = Ranges.create((BSLParser.StatementContext) child);
           if (regionsRanges.stream().noneMatch(regionRange ->
