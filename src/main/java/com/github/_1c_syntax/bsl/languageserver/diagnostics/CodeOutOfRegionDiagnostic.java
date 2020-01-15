fc476d3e4bb48d44d1e93824594e6015a2e238c0
Alexey Sosnoviy <int-it@yandex.ru>
2020-01-15 23:38:56 +0300

    fix fp outOfRegion empty file #804

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/CodeOutOfRegionDiagnostic.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/CodeOutOfRegionDiagnostic.java
@@ -65,14 +65,14 @@ public class CodeOutOfRegionDiagnostic extends AbstractVisitorDiagnostic {
     regionsRanges.clear();
 
     // если областей нет, то и смысла дальше анализировть тоже нет
-    if (regions.isEmpty() && ctx.getChildCount() > 0) {
+    if (regions.isEmpty() && !ctx.getTokens().isEmpty()) {
       diagnosticStorage.addDiagnostic(ctx);
       return ctx;
-    } else {
-      regions.forEach(region ->
-        regionsRanges.add(Ranges.create(region))
-      );
     }
+
+    regions.forEach(region ->
+      regionsRanges.add(Ranges.create(region))
+    );
 
     return super.visitFile(ctx);
 
