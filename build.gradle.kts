44ec16005925dbe7dca537397cb13be77526c9d1
Tymko Oleg <olegtymko@yandex.ru>
2020-01-23 22:28:32 +0700

    ап mdclasses

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,7 +100,7 @@ dependencies {
         exclude("org.glassfish", "javax.json")
     }
 
-    implementation("com.github.1c-syntax:mdclasses:0.2.1")
+    implementation("com.github.1c-syntax:mdclasses:b503ae7b77")
 
     compileOnly("org.projectlombok", "lombok", lombok.version)
 
