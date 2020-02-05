ecba37dbf1f331a5475fdc813b71ab3101ab2a29
Oleg Tymko <olegtymko@yandex.ru>
2020-02-05 19:39:57 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,7 +100,7 @@ dependencies {
         exclude("org.glassfish", "javax.json")
     }
 
-    implementation("com.github.1c-syntax:mdclasses:0.2.2")
+    implementation("com.github.1c-syntax:mdclasses:0.2.3")
 
     compileOnly("org.projectlombok", "lombok", lombok.version)
 
