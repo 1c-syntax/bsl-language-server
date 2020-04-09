03b0563b6456756d8f6cedae3e3fe1c87b8c1247
Aleksandr Ponkratov <ponkratov.aleks@mail.ru>
2020-04-09 15:31:47 +0300

    Добавлена новая диагностика

---
+++ docs/en/diagnostics/DeprecatedCurrentDate.md
@@ -0,0 +1,52 @@
+# Using of the deprecated method "CurrentDate" (DeprecatedCurrentDate)
+
+| Type | Scope | Severity | Activated<br/>by default | Minutes<br/>to fix | Tags |
+| :-: | :-: | :-: | :-: | :-: | :-: |
+| `Error` | `BSL` | `Major` | `Yes` | `1` | `standard`<br/>`badpractice`<br/>`unpredictable` |
+
+<!-- Блоки выше заполняются автоматически, не трогать -->
+## Description
+
+Method "CurrentDate" is deprecated. Use "CurrentSessionDate" instead.
+
+## Examples
+<!-- В данном разделе приводятся примеры, на которые диагностика срабатывает, а также можно привести пример, как можно исправить ситуацию -->
+
+Incorrect:
+
+```bsl
+OperationDate = CurrentDate();
+```
+
+
+Correct:
+
+```bsl
+OperationDate = CurrentSessionDate();
+```
+
+## Sources
+<!-- Необходимо указывать ссылки на все источники, из которых почерпнута информация для создания диагностики -->
+<!-- Примеры источников
+
+* Источник: [Стандарт: Тексты модулей](https://its.1c.ru/db/v8std#content:456:hdoc)
+* Полезная информаця: [Отказ от использования модальных окон](https://its.1c.ru/db/metod8dev#content:5272:hdoc)
+* Источник: [Cognitive complexity, ver. 1.4](https://www.sonarsource.com/docs/CognitiveComplexity.pdf) -->
+
+* Reference: [Metadata creation and change. Work in different timezones](https://its.1c.ru/db/v8std/content/643/hdoc)
+
+## Snippets
+
+<!-- Блоки ниже заполняются автоматически, не трогать -->
+### Diagnostic ignorance in code
+
+```bsl
+// BSLLS:DeprecatedCurrentDate-off
+// BSLLS:DeprecatedCurrentDate-on
+```
+
+### Parameter for config
+
+```json
+"DeprecatedCurrentDate": false
+```
---
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/DeprecatedCurrentDateDiagnosticTest.java
@@ -0,0 +1,75 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2020
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package com.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import com.github._1c_syntax.bsl.languageserver.context.DocumentContext;
+import org.eclipse.lsp4j.CodeAction;
+import org.eclipse.lsp4j.Diagnostic;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.github._1c_syntax.bsl.languageserver.util.Assertions.assertThat;
+
+class DeprecatedCurrentDateDiagnosticTest extends AbstractDiagnosticTest<DeprecatedCurrentDateDiagnostic> {
+  DeprecatedCurrentDateDiagnosticTest() {
+    super(DeprecatedCurrentDateDiagnostic.class);
+  }
+
+  @Test
+  void test() {
+
+    List<Diagnostic> diagnostics = getDiagnostics();
+
+    assertThat(diagnostics).hasSize(2);
+    assertThat(diagnostics, true)
+      .hasRange(2, 19, 2, 30)
+      .hasRange(11, 16, 11, 27);
+
+  }
+
+  @Test
+  void testQuickFixes() {
+
+    final DocumentContext documentContext = getDocumentContext();
+    List<Diagnostic> diagnostics = getDiagnostics();
+    final Diagnostic diagnostic = diagnostics.get(0);
+    final String replacementText = "ТекущаяДатаСеанса";
+
+    List<CodeAction> QuickFixes = getQuickFixes(diagnostic);
+    assertThat(QuickFixes).hasSize(1);
+
+    final CodeAction quickFix = QuickFixes.get(0);
+
+    assertThat(quickFix)
+      .of(diagnosticInstance)
+      .in(documentContext)
+      .fixes(diagnostic);
+
+    assertThat(quickFix)
+      .in(documentContext)
+      .hasChanges(1)
+      .hasNewText(replacementText);
+
+  }
+
+}
