e439bd4592ad9128619e750f03db20bb4fa659e1
Oleg Tymko <olegtymko@yandex.ru>
2020-04-11 21:12:37 +0700

    исправление имени скрипта bechmark

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,70 +1,70 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1586613941986,
-  "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
-  "entries": {
-    "BSL LS perfomance measurement (SSL 3.1)": [
-      {
-        "commit": {
-          "author": {
-            "email": "nixel2007@gmail.com",
-            "name": "Nikita Gryzlov",
-            "username": "nixel2007"
+    "lastUpdate": 1586613941986,
+    "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
+    "entries": {
+      "BSL LS perfomance measurement (SSL 3.1)": [
+        {
+          "commit": {
+            "author": {
+              "email": "nixel2007@gmail.com",
+              "name": "Nikita Gryzlov",
+              "username": "nixel2007"
+            },
+            "committer": {
+              "email": "noreply@github.com",
+              "name": "GitHub",
+              "username": "web-flow"
+            },
+            "distinct": true,
+            "id": "6653a3b708904ab9f45c80c2a784b22d7e30c15b",
+            "message": "Merge pull request #1031 from 1c-syntax/feature/benchmark-badge\n\nБейдж замеров производительности",
+            "timestamp": "2020-04-11T12:18:54+03:00",
+            "tree_id": "f63e3dc9fc8765d9635d244826c692e49e3a0220",
+            "url": "https://github.com/1c-syntax/bsl-language-server/commit/6653a3b708904ab9f45c80c2a784b22d7e30c15b"
           },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "6653a3b708904ab9f45c80c2a784b22d7e30c15b",
-          "message": "Merge pull request #1031 from 1c-syntax/feature/benchmark-badge\n\nБейдж замеров производительности",
-          "timestamp": "2020-04-11T12:18:54+03:00",
-          "tree_id": "f63e3dc9fc8765d9635d244826c692e49e3a0220",
-          "url": "https://github.com/1c-syntax/bsl-language-server/commit/6653a3b708904ab9f45c80c2a784b22d7e30c15b"
+          "date": 1586597055409,
+          "tool": "pytest",
+          "benches": [
+            {
+              "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+              "value": 63.476545333862305,
+              "unit": "sec",
+              "range": "stddev: 0",
+              "extra": "mean: 63.476545333862305 sec\nrounds: 1"
+            }
+          ]
         },
-        "date": 1586597055409,
-        "tool": "pytest",
-        "benches": [
-          {
-            "name": ".github/scripts/bechmark.py::test_analyze_ssl31",
-            "value": 63.476545333862305,
-            "unit": "sec",
-            "range": "stddev: 0",
-            "extra": "mean: 63.476545333862305 sec\nrounds: 1"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "nixel2007@gmail.com",
-            "name": "Nikita Gryzlov",
-            "username": "nixel2007"
+        {
+          "commit": {
+            "author": {
+              "email": "nixel2007@gmail.com",
+              "name": "Nikita Gryzlov",
+              "username": "nixel2007"
+            },
+            "committer": {
+              "email": "noreply@github.com",
+              "name": "GitHub",
+              "username": "web-flow"
+            },
+            "distinct": true,
+            "id": "8ac7c0591539f07e3c6d04b14771c1650ce33397",
+            "message": "Merge pull request #1033 from otymko/feature/fix-benchmark\n\nFix benchmark",
+            "timestamp": "2020-04-11T16:59:45+03:00",
+            "tree_id": "6f84e553b2d863245d2ef8607e4dff865fea0878",
+            "url": "https://github.com/1c-syntax/bsl-language-server/commit/8ac7c0591539f07e3c6d04b14771c1650ce33397"
           },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "8ac7c0591539f07e3c6d04b14771c1650ce33397",
-          "message": "Merge pull request #1033 from otymko/feature/fix-benchmark\n\nFix benchmark",
-          "timestamp": "2020-04-11T16:59:45+03:00",
-          "tree_id": "6f84e553b2d863245d2ef8607e4dff865fea0878",
-          "url": "https://github.com/1c-syntax/bsl-language-server/commit/8ac7c0591539f07e3c6d04b14771c1650ce33397"
-        },
-        "date": 1586613940488,
-        "tool": "pytest",
-        "benches": [
-          {
-            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
-            "value": 72.43926119804382,
-            "unit": "sec",
-            "range": "stddev: 0",
-            "extra": "mean: 72.43926119804382 sec\nrounds: 1"
-          }
-        ]
-      }
-    ]
+          "date": 1586613940488,
+          "tool": "pytest",
+          "benches": [
+            {
+              "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+              "value": 72.43926119804382,
+              "unit": "sec",
+              "range": "stddev: 0",
+              "extra": "mean: 72.43926119804382 sec\nrounds: 1"
+            }
+          ]
+        }
+      ]
+    }
   }
-}
