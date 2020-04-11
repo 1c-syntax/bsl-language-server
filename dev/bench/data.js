fe685796eb3598ee2efecd91e323a467bcde99bf
github-action-benchmark <github@users.noreply.github.com>
2020-04-11 14:40:27 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 5eebe6dc36fb11482ceea0279253a9f1cb9f7f08

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,70 +1,101 @@
 window.BENCHMARK_DATA = {
-    "lastUpdate": 1586613941986,
-    "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
-    "entries": {
-      "BSL LS perfomance measurement (SSL 3.1)": [
-        {
-          "commit": {
-            "author": {
-              "email": "nixel2007@gmail.com",
-              "name": "Nikita Gryzlov",
-              "username": "nixel2007"
-            },
-            "committer": {
-              "email": "noreply@github.com",
-              "name": "GitHub",
-              "username": "web-flow"
-            },
-            "distinct": true,
-            "id": "6653a3b708904ab9f45c80c2a784b22d7e30c15b",
-            "message": "Merge pull request #1031 from 1c-syntax/feature/benchmark-badge\n\nБейдж замеров производительности",
-            "timestamp": "2020-04-11T12:18:54+03:00",
-            "tree_id": "f63e3dc9fc8765d9635d244826c692e49e3a0220",
-            "url": "https://github.com/1c-syntax/bsl-language-server/commit/6653a3b708904ab9f45c80c2a784b22d7e30c15b"
+  "lastUpdate": 1586616027728,
+  "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
+  "entries": {
+    "BSL LS perfomance measurement (SSL 3.1)": [
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Gryzlov",
+            "username": "nixel2007"
           },
-          "date": 1586597055409,
-          "tool": "pytest",
-          "benches": [
-            {
-              "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
-              "value": 63.476545333862305,
-              "unit": "sec",
-              "range": "stddev: 0",
-              "extra": "mean: 63.476545333862305 sec\nrounds: 1"
-            }
-          ]
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6653a3b708904ab9f45c80c2a784b22d7e30c15b",
+          "message": "Merge pull request #1031 from 1c-syntax/feature/benchmark-badge\n\nБейдж замеров производительности",
+          "timestamp": "2020-04-11T12:18:54+03:00",
+          "tree_id": "f63e3dc9fc8765d9635d244826c692e49e3a0220",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/6653a3b708904ab9f45c80c2a784b22d7e30c15b"
         },
-        {
-          "commit": {
-            "author": {
-              "email": "nixel2007@gmail.com",
-              "name": "Nikita Gryzlov",
-              "username": "nixel2007"
-            },
-            "committer": {
-              "email": "noreply@github.com",
-              "name": "GitHub",
-              "username": "web-flow"
-            },
-            "distinct": true,
-            "id": "8ac7c0591539f07e3c6d04b14771c1650ce33397",
-            "message": "Merge pull request #1033 from otymko/feature/fix-benchmark\n\nFix benchmark",
-            "timestamp": "2020-04-11T16:59:45+03:00",
-            "tree_id": "6f84e553b2d863245d2ef8607e4dff865fea0878",
-            "url": "https://github.com/1c-syntax/bsl-language-server/commit/8ac7c0591539f07e3c6d04b14771c1650ce33397"
+        "date": 1586597055409,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 63.476545333862305,
+            "unit": "sec",
+            "range": "stddev: 0",
+            "extra": "mean: 63.476545333862305 sec\nrounds: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Gryzlov",
+            "username": "nixel2007"
           },
-          "date": 1586613940488,
-          "tool": "pytest",
-          "benches": [
-            {
-              "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
-              "value": 72.43926119804382,
-              "unit": "sec",
-              "range": "stddev: 0",
-              "extra": "mean: 72.43926119804382 sec\nrounds: 1"
-            }
-          ]
-        }
-      ]
-    }
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8ac7c0591539f07e3c6d04b14771c1650ce33397",
+          "message": "Merge pull request #1033 from otymko/feature/fix-benchmark\n\nFix benchmark",
+          "timestamp": "2020-04-11T16:59:45+03:00",
+          "tree_id": "6f84e553b2d863245d2ef8607e4dff865fea0878",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/8ac7c0591539f07e3c6d04b14771c1650ce33397"
+        },
+        "date": 1586613940488,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 72.43926119804382,
+            "unit": "sec",
+            "range": "stddev: 0",
+            "extra": "mean: 72.43926119804382 sec\nrounds: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Gryzlov",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5eebe6dc36fb11482ceea0279253a9f1cb9f7f08",
+          "message": "Merge pull request #1034 from 1c-syntax/otymko-patch-round\n\nУвеличение кругов bench с 1 до 3",
+          "timestamp": "2020-04-11T17:32:47+03:00",
+          "tree_id": "39fc6a8b65b34b0e9b16e8ca2795c90eb6ce93ab",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/5eebe6dc36fb11482ceea0279253a9f1cb9f7f08"
+        },
+        "date": 1586616026290,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 63.76906005541483,
+            "unit": "sec",
+            "range": "stddev: 1.2978114728391386",
+            "extra": "mean: 63.76906005541483 sec\nrounds: 3"
+          }
+        ]
+      }
+    ]
   }
+}
