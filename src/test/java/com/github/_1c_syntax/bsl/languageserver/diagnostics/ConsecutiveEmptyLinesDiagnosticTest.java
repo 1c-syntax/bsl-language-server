06227acbb8ebb522e2f58a098d17f37c84c8bd58
Alexey Sosnoviy <int-it@yandex.ru>
2020-04-14 00:56:58 +0300

    Магические тесты от Артура

--- src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/ConsecutiveEmptyLinesDiagnosticTest.java
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/ConsecutiveEmptyLinesDiagnosticTest.java
@@ -21,6 +21,7 @@
  */
 package com.github._1c_syntax.bsl.languageserver.diagnostics;
 
+import com.github._1c_syntax.bsl.languageserver.util.TestUtils;
 import org.eclipse.lsp4j.CodeAction;
 import org.eclipse.lsp4j.Diagnostic;
 import org.junit.jupiter.api.Test;
@@ -83,6 +84,175 @@ class ConsecutiveEmptyLinesDiagnosticTest extends AbstractDiagnosticTest<Consecu
     assertThat(diagnostics, true)
       .hasRange(37, 0, 39, 0)
     ;
+  }
+
+
+  @Test
+  void test_EmptyTwoFirstLines() {
+
+    configure(1);
+
+    String module = "  \n" +
+      "\n" +
+      "КонецПроцедуры";
+
+    List<Diagnostic> diagnostics = getDiagnostics(TestUtils.getDocumentContext(module));
+
+    assertThat(diagnostics, true)
+      .hasSize(1)
+      .hasRange(0, 0, 1, 0)
+    ;
+  }
+
+  @Test
+  void test_EmptyThreeFirstLines() {
+
+    configure(1);
+
+    String module = "  \n" +
+      "\n" +
+      "\n" +
+      "КонецПроцедуры";
+
+    List<Diagnostic> diagnostics = getDiagnostics(TestUtils.getDocumentContext(module));
+
+    assertThat(diagnostics, true)
+      .hasSize(1)
+      .hasRange(1, 0, 2, 0)
+    ;
+  }
+
+  @Test
+  void test_EmptyTwoInnerLines() {
+
+    configure(1);
+
+    String module = "Процедура Первая()\n" +
+      "\n" +
+      "\n" +
+      "КонецПроцедуры";
+
+    List<Diagnostic> diagnostics = getDiagnostics(TestUtils.getDocumentContext(module));
+
+    assertThat(diagnostics, true)
+      .hasSize(1)
+      .hasRange(1, 0, 2, 0)
+    ;
+  }
+
+  @Test
+  void test_EmptyTwoInnerLinesWithSpaces() {
+
+    configure(1);
+
+    String module = "Процедура Первая()  \n" +
+      " \n" +
+      " \n" +
+      "КонецПроцедуры";
+
+    List<Diagnostic> diagnostics = getDiagnostics(TestUtils.getDocumentContext(module));
+
+    assertThat(diagnostics, true)
+      .hasSize(1)
+      .hasRange(1, 0, 2, 0)
+    ;
+  }
+
+  @Test
+  void test_WorseEmptyTwoInnerLines() {
+
+    configure(1);
+
+    String module = "Процедура Первая()  \n" +
+      "  \n" +
+      "  Метод1(); //комментарии  \n" +
+      "\n" +
+      "  \n" +
+      "КонецПроцедуры";
+
+    List<Diagnostic> diagnostics = getDiagnostics(TestUtils.getDocumentContext(module));
+
+    assertThat(diagnostics, true)
+      .hasSize(1)
+      .hasRange(3, 0, 4, 0)
+    ;
+  }
+
+  @Test
+  void test_EmptyThreeInnerLines() {
+
+    configure(1);
+
+    String module = "Процедура Первая()\n" +
+      "\n" +
+      "\n" +
+      "\n" +
+      "КонецПроцедуры";
+
+    List<Diagnostic> diagnostics = getDiagnostics(TestUtils.getDocumentContext(module));
+
+    assertThat(diagnostics, true)
+      .hasSize(1)
+      .hasRange(2, 0, 3, 0)
+    ;
+  }
+
+  @Test
+  void test_EmptyLastLines() {
+
+    configure(1);
+
+    String module = "Перем А;\n" +
+      "\n";
+
+    List<Diagnostic> diagnostics = getDiagnostics(TestUtils.getDocumentContext(module));
+
+//    assertThat(diagnostics, true) // А почему этот тест должен сработать?
+//      .hasSize(1)
+//      .hasRange(1, 0, 2, 0)
+    ;
+  }
+
+  @Test
+  void test_ConfigureEmptyLineParam() {
+
+    configure(2);
+
+    String module = "Процедура Первая()\n" +
+      "\n" +
+      "\n" +
+      "\n" +
+      "КонецПроцедуры";
+
+    List<Diagnostic> diagnostics = getDiagnostics(TestUtils.getDocumentContext(module));
+
+    assertThat(diagnostics, true)
+      .hasRange(2, 0, 3, 0)
+      .hasSize(1)
+    ;
+  }
+
+  @Test
+  void test_ConfigureEmptyLineParamNoIssue() {
+
+    configure(2);
+
+    String module = "Процедура Первая()\n" +
+      "\n" +
+      "\n" +
+      "КонецПроцедуры";
+
+    List<Diagnostic> diagnostics = getDiagnostics(TestUtils.getDocumentContext(module));
+
+    assertThat(diagnostics, true)
+      .hasSize(0)
+    ;
+  }
+
+  private void configure(int maxEmptyLineCount) {
+    Map<String, Object> configuration = diagnosticInstance.getInfo().getDefaultConfiguration();
+    configuration.put("maxEmptyLineCount", maxEmptyLineCount);
+    diagnosticInstance.configure(configuration);
   }
 
   @Test
