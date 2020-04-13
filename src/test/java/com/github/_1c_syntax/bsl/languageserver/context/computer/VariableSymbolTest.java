6d046c7ff672041e9d7fe7808720ca4aa5ecf607
Aleksandr Ponkratov <ponkratov.aleks@mail.ru>
2020-04-13 12:28:49 +0300

    Добавлен тест VariableSymbol

---
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/context/computer/VariableSymbolTest.java
@@ -0,0 +1,60 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2020
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package com.github._1c_syntax.bsl.languageserver.context.computer;
+
+import com.github._1c_syntax.bsl.languageserver.context.DocumentContext;
+import com.github._1c_syntax.bsl.languageserver.context.symbol.VariableSymbol;
+import com.github._1c_syntax.bsl.languageserver.util.TestUtils;
+import com.github._1c_syntax.bsl.languageserver.utils.Ranges;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VariableSymbolTest {
+
+  @Test
+  void testVariableDescription() {
+
+    DocumentContext documentContext = TestUtils.getDocumentContextFromFile("./src/test/resources/providers/variableSymbol.bsl");
+    List<VariableSymbol> variableSymbols = documentContext.getSymbolTree().getVariables();
+
+    assertThat(variableSymbols).hasSize(4);
+
+    assertThat(variableSymbols)
+      .filteredOn(variableSymbol -> variableSymbol.getDescription().isEmpty())
+      .hasSize(2)
+      .anyMatch(variableSymbol -> variableSymbol.getRange().equals(Ranges.create(1, 6, 1, 27)))
+      .anyMatch(variableSymbol -> variableSymbol.getRange().equals(Ranges.create(10, 6, 10, 34)))
+    ;
+
+    assertThat(variableSymbols)
+      .filteredOn(variableSymbol -> variableSymbol.getDescription().isPresent())
+      .hasSize(2)
+      .anyMatch(variableSymbol -> variableSymbol.getRange().equals(Ranges.create(4, 6, 4, 32)))
+      .anyMatch(variableSymbol -> variableSymbol.getRange().equals(Ranges.create(6, 6, 6, 33)))
+    ;
+
+  }
+
+}
