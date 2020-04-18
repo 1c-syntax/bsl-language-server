2958098074abd213efdc02f2bcc426db14a902a4
Alexey Sosnoviy <int-it@yandex.ru>
2020-04-18 23:27:46 +0300

    CommonModuleNameCashedDiagnostic

---
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/CommonModuleNameCachedDiagnostic.java
@@ -0,0 +1,59 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2020
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package com.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticInfo;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticTag;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
+import com.github._1c_syntax.mdclasses.mdo.CommonModule;
+import com.github._1c_syntax.mdclasses.metadata.additional.ReturnValueReuse;
+
+import java.util.regex.Pattern;
+
+@DiagnosticMetadata(
+  type = DiagnosticType.CODE_SMELL,
+  severity = DiagnosticSeverity.INFO,
+  minutesToFix = 1,
+  tags = {
+    DiagnosticTag.STANDARD
+  }
+
+)
+public class CommonModuleNameCachedDiagnostic extends AbstractCommonModuleNameDiagnostic {
+  public CommonModuleNameCachedDiagnostic(DiagnosticInfo info) {
+    super(info);
+    pattern = Pattern.compile(
+      "^.*повтисп|^.*сached",
+      Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE
+    );
+
+  }
+
+  @Override
+  protected boolean flagsCheck(CommonModule commonModule) {
+    return commonModule.getReturnValuesReuse() == ReturnValueReuse.DURING_REQUEST
+      || commonModule.getReturnValuesReuse() == ReturnValueReuse.DURING_SESSION;
+  }
+
+}
