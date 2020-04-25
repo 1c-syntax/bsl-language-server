f687ef3626081d41c1d5444d2d6c337c1f373679
Tymko Oleg <olegtymko@yandex.ru>
2020-04-26 01:23:36 +0700

    Исключение не примитивов, дополнена документация на ru, переименование имени файла кейса для теста

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/FunctionReturnsSamePrimitiveDiagnostic.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/FunctionReturnsSamePrimitiveDiagnostic.java
@@ -1,5 +1,27 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2020
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
 package com.github._1c_syntax.bsl.languageserver.diagnostics;
 
+import com.github._1c_syntax.bsl.languageserver.context.symbol.MethodSymbol;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticInfo;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
@@ -8,35 +30,31 @@ import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticT
 import com.github._1c_syntax.bsl.languageserver.utils.Ranges;
 import com.github._1c_syntax.bsl.languageserver.utils.RelatedInformation;
 import com.github._1c_syntax.bsl.languageserver.utils.Trees;
-import com.github._1c_syntax.bsl.parser.BSLLexer;
 import com.github._1c_syntax.bsl.parser.BSLParser;
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.checkerframework.checker.nullness.Opt;
-import org.eclipse.lsp4j.DiagnosticRelatedInformation;
+import org.eclipse.lsp4j.Range;
 
-import javax.tools.Diagnostic;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @DiagnosticMetadata(
   type = DiagnosticType.ERROR,
-  severity = DiagnosticSeverity.INFO,
-  minutesToFix = 1,
+  severity = DiagnosticSeverity.MAJOR,
+  minutesToFix = 5,
   tags = {
+    DiagnosticTag.DESIGN,
     DiagnosticTag.BADPRACTICE
   }
 
 )
 public class FunctionReturnsSamePrimitiveDiagnostic extends AbstractVisitorDiagnostic {
+
+  private static final String KEY_MESSAGE = "diagnosticMessageReturnStatement";
+
   public FunctionReturnsSamePrimitiveDiagnostic(DiagnosticInfo info) {
     super(info);
   }
@@ -44,25 +62,49 @@ public class FunctionReturnsSamePrimitiveDiagnostic extends AbstractVisitorDiagn
   @Override
   public ParseTree visitFunction(BSLParser.FunctionContext ctx) {
     var tree = Trees.findAllRuleNodes(ctx, BSLParser.RULE_returnStatement);
-    if (tree.size() > 1){
-      var set = tree.stream()
+    if (tree.size() > 1) {
+      var expressions = tree.stream()
         .map(BSLParser.ReturnStatementContext.class::cast)
         .map(BSLParser.ReturnStatementContext::expression)
         .flatMap(Stream::ofNullable)
-        .map(BSLParser.ExpressionContext::getText)
-        .collect(Collectors.toSet());
-      if (set.size() == 1) {
-        var relatedInformation = tree.stream()
-          .map(BSLParser.ReturnStatementContext.class::cast)
-          .map(statement ->
-            RelatedInformation.create(
-              documentContext.getUri(),
-              Ranges.create(statement.getStart()),
-              info.getMessage()))
-          .collect(Collectors.toList());
-        diagnosticStorage.addDiagnostic(ctx.funcDeclaration(), info.getMessage(), relatedInformation);
-      }
+        .collect(Collectors.toList());
+
+      expressions.stream()
+        .map(expression -> Trees.findAllRuleNodes(expression, BSLParser.RULE_complexIdentifier))
+        .filter(Collection::isEmpty)
+        .findAny()
+        .ifPresent(
+          parseTrees -> checkPrimitiveValue(ctx, tree, expressions)
+        );
     }
     return ctx;
   }
+
+  private void checkPrimitiveValue(
+    BSLParser.FunctionContext ctx, Collection<ParseTree> tree, List<BSLParser.ExpressionContext> expressions) {
+
+    var set = expressions.stream().map(BSLParser.ExpressionContext::getText).collect(Collectors.toSet());
+    if (set.size() == 1) {
+      var relatedInformation = tree.stream()
+        .map(BSLParser.ReturnStatementContext.class::cast)
+        .map(statement -> RelatedInformation.create(
+          documentContext.getUri(),
+          Ranges.create(statement.getStart()),
+          info.getResourceString(KEY_MESSAGE)))
+        .collect(Collectors.toList());
+      diagnosticStorage.addDiagnostic(getSubNameRange(ctx), info.getMessage(), relatedInformation);
+    }
+
+  }
+
+  private Range getSubNameRange(ParserRuleContext ctx) {
+    return Optional.ofNullable(Trees.getAncestorByRuleIndex(ctx, BSLParser.RULE_sub))
+      .map(BSLParser.SubContext.class::cast)
+      .map(context -> documentContext.getSymbolTree().getMethodSymbol(context))
+      .filter(Optional::isPresent)
+      .map(Optional::get)
+      .map(MethodSymbol::getSubNameRange)
+      .orElse(Ranges.create(ctx.getStart()));
+  }
+
 }
