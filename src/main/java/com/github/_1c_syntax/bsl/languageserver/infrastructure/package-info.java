df5acbaf31a9c374f8e70e024b3c44d0ad5e5bf3
Nikita Gryzlov <nixel2007@gmail.com>
2020-08-04 13:09:01 +0300

    Javadoc

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/infrastructure/package-info.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/infrastructure/package-info.java
@@ -19,18 +19,10 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with BSL Language Server.
  */
+/**
+ * Spring-специфичные классы для настройки внутренней инфраструктуры уровня приложения.
+ */
+@ParametersAreNonnullByDefault
 package com.github._1c_syntax.bsl.languageserver.infrastructure;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-/**
- * Spring-конфигурация для управления включением/отключением фоновых заданий.
- * <p />
- * См. {@link Configuration}
- */
-@Configuration
-@EnableScheduling
-@ConditionalOnProperty(prefix = "app.scheduling", name="enabled", havingValue="true", matchIfMissing = true)
-public class SchedulingConfiguration {}
+import javax.annotation.ParametersAreNonnullByDefault;
