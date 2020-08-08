d85cdfcb9b2bf94d7fe4b5b36c9188dd72e013d0
qtLex <skikaha@yandex.ru>
2020-08-08 16:57:39 +0300

    code action complite

---
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/codeactions/DisableDiagnosticTriggeringSupplierTest.java
@@ -0,0 +1,203 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2020
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package com.github._1c_syntax.bsl.languageserver.codeactions;
+
+import com.github._1c_syntax.bsl.languageserver.context.DocumentContext;
+import com.github._1c_syntax.bsl.languageserver.providers.CodeActionProvider;
+import com.github._1c_syntax.bsl.languageserver.util.TestUtils;
+import com.github._1c_syntax.bsl.languageserver.utils.Ranges;
+import org.eclipse.lsp4j.CodeAction;
+import org.eclipse.lsp4j.CodeActionContext;
+import org.eclipse.lsp4j.CodeActionParams;
+import org.eclipse.lsp4j.Command;
+import org.eclipse.lsp4j.Range;
+import org.eclipse.lsp4j.TextDocumentIdentifier;
+import org.eclipse.lsp4j.jsonrpc.messages.Either;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class DisableDiagnosticTriggeringSupplierTest {
+
+  private final CodeActionProvider codeActionProvider;
+
+  public DisableDiagnosticTriggeringSupplierTest() {
+
+    List<CodeActionSupplier> codeActionSuppliers = new ArrayList<>();
+    codeActionSuppliers.add(new DisableDiagnosticTriggeringSupplier());
+    codeActionProvider = new CodeActionProvider(codeActionSuppliers);
+
+  }
+
+  @Test
+  void testGetCodeActions() {
+
+    DocumentContext documentContext = TestUtils.getDocumentContextFromFile(
+      "./src/test/resources/suppliers/disableDiagnosticTriggering.bsl"
+    );
+
+    TextDocumentIdentifier textDocumentIdentifier = new TextDocumentIdentifier(documentContext.getUri().toString());
+
+    CodeActionContext codeActionContext = new CodeActionContext();
+    codeActionContext.setDiagnostics(documentContext.getDiagnostics());
+
+    CodeActionParams params = new CodeActionParams();
+    params.setRange(new Range());
+    params.setTextDocument(textDocumentIdentifier);
+    params.setContext(codeActionContext);
+
+    List<Either<Command, CodeAction>> codeActions = codeActionProvider.getCodeActions(params, documentContext);
+
+    assertThat(codeActions)
+      .hasSize(11)
+      .extracting(Either::getRight)
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable all diagnostic in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable NumberOfValuesInStructureConstructor in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable ExportVariables in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable IfElseDuplicatedCondition in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable CanonicalSpellingKeywords in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable CodeOutOfRegion in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable FunctionShouldHaveReturn in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable IfElseIfEndsWithElse in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable MagicNumber in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable MissingSpace in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable MissingVariablesDescription in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable all diagnostic in file"));
+  }
+
+  @Test
+  void testGetCodeActionsOneLine() {
+
+    DocumentContext documentContext = TestUtils.getDocumentContextFromFile(
+      "./src/test/resources/suppliers/disableDiagnosticTriggering.bsl"
+    );
+
+    TextDocumentIdentifier textDocumentIdentifier = new TextDocumentIdentifier(documentContext.getUri().toString());
+
+    Range range = Ranges.create(53, 0, 53, 30);
+
+    CodeActionContext codeActionContext = new CodeActionContext();
+    codeActionContext.setDiagnostics(
+      documentContext
+        .getDiagnostics()
+        .stream()
+        .filter(diagnostic ->
+          Ranges.containsRange(range, diagnostic.getRange())
+        )
+      .collect(Collectors.toList())
+    );
+
+    CodeActionParams params = new CodeActionParams();
+    params.setRange(range);
+    params.setTextDocument(textDocumentIdentifier);
+    params.setContext(codeActionContext);
+
+    List<Either<Command, CodeAction>> codeActions = codeActionProvider.getCodeActions(params, documentContext);
+
+    assertThat(codeActions)
+      .hasSize(6)
+      .extracting(Either::getRight)
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable MagicNumber in line"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable MissingSpace in line"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable MagicNumber in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable MissingSpace in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable all diagnostic in line"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable all diagnostic in file"));
+
+  }
+
+  @Test
+  void testGetCodeActionsRegion() {
+
+    DocumentContext documentContext = TestUtils.getDocumentContextFromFile(
+      "./src/test/resources/suppliers/disableDiagnosticTriggering.bsl"
+    );
+
+    TextDocumentIdentifier textDocumentIdentifier = new TextDocumentIdentifier(documentContext.getUri().toString());
+
+    Range range = Ranges.create(53, 0, 56, 38);
+
+    CodeActionContext codeActionContext = new CodeActionContext();
+    codeActionContext.setDiagnostics(
+      documentContext
+        .getDiagnostics()
+        .stream()
+        .filter(diagnostic -> Ranges.containsRange(range, diagnostic.getRange()))
+        .collect(Collectors.toList())
+    );
+
+    CodeActionParams params = new CodeActionParams();
+    params.setRange(range);
+    params.setTextDocument(textDocumentIdentifier);
+    params.setContext(codeActionContext);
+
+    List<Either<Command, CodeAction>> codeActions = codeActionProvider.getCodeActions(params, documentContext);
+
+    assertThat(codeActions)
+      .hasSize(8)
+      .extracting(Either::getRight)
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable MagicNumber in range"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable MissingSpace in range"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable MagicNumber in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable MissingSpace in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable all diagnostic in range"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable all diagnostic in file"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable CanonicalSpellingKeywords in range"))
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable CanonicalSpellingKeywords in file"));
+
+  }
+
+  @Test
+  void testGetCodeActionsEmptyFile() {
+
+    DocumentContext documentContext = TestUtils.getDocumentContextFromFile(
+      "./src/test/resources/suppliers/disableDiagnosticTriggeringEmpty.bsl"
+    );
+
+    TextDocumentIdentifier textDocumentIdentifier = new TextDocumentIdentifier(documentContext.getUri().toString());
+
+    CodeActionContext codeActionContext = new CodeActionContext();
+    codeActionContext.setDiagnostics(documentContext.getDiagnostics());
+
+    CodeActionParams params = new CodeActionParams();
+    params.setRange(new Range());
+    params.setTextDocument(textDocumentIdentifier);
+    params.setContext(codeActionContext);
+
+    List<Either<Command, CodeAction>> codeActions = codeActionProvider.getCodeActions(params, documentContext);
+
+    assertThat(codeActions)
+      .hasSize(1)
+      .extracting(Either::getRight)
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Disable all diagnostic in file"));
+
+  }
+}
