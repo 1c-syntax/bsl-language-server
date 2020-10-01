4b590cfc2530c98476e22df1d6036fc7d9b09237
github-action-benchmark <github@users.noreply.github.com>
2020-10-01 11:49:47 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for d997a83afc18eeaee086bd9a7ab90702714ae090

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1601552525876,
+  "lastUpdate": 1601552987166,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -5580,6 +5580,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 0.3610778795742331",
             "extra": "mean: 76.99180952707927 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Gryzlov",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d997a83afc18eeaee086bd9a7ab90702714ae090",
+          "message": "Merge pull request #1372 from 1c-syntax/fix/metadataLongName",
+          "timestamp": "2020-10-01T14:36:03+03:00",
+          "tree_id": "85d15474cb0f2cdbb3cbbfc7ddb0c6a65bc5ef6f",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/d997a83afc18eeaee086bd9a7ab90702714ae090"
+        },
+        "date": 1601552983805,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 75.83348973592122,
+            "unit": "sec",
+            "range": "stddev: 2.613843930190249",
+            "extra": "mean: 75.83348973592122 sec\nrounds: 3"
           }
         ]
       }
