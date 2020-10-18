75b4c1cecb2b861a8bb5b6fcacbf2b91698246b8
Nikita Gryzlov <nixel2007@gmail.com>
2020-10-18 21:09:50 +0300

    Заготовки тестов ReferenceProvider

---
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/providers/ReferencesProviderTest.java
@@ -0,0 +1,33 @@
+package com.github._1c_syntax.bsl.languageserver.providers;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferencesProviderTest {
+
+  @Test
+  @Disabled
+  void testMethodsFromCommonModule() {
+    // todo
+  }
+
+  @Test
+  @Disabled
+  void testCantGoToPrivateMethodsFromCommonModule() {
+    // todo
+  }
+
+  @Test
+  @Disabled
+  void testMethodsFromManagerModule() {
+    // todo
+  }
+
+  @Test
+  @Disabled
+  void testLocalMethods() {
+    // todo
+  }
+}
