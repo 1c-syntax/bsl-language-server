7d37c52f892aef52e7aa5e3597786bf042a90e4a
github-action-benchmark <github@users.noreply.github.com>
2020-11-08 09:06:43 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 5770f332ff9bac731a457ae5b857604549309df2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1604758412669,
+  "lastUpdate": 1604826402720,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -6603,6 +6603,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 0.8187941003165571",
             "extra": "mean: 76.86204361915588 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Gryzlov",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5770f332ff9bac731a457ae5b857604549309df2",
+          "message": "Merge pull request #1416 from 1c-syntax/feature/ordinaryAppSupportDiagnostic",
+          "timestamp": "2020-11-08T11:42:56+03:00",
+          "tree_id": "7a0e9fd9eb05769b9a272c0992dd724dee6ada48",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/5770f332ff9bac731a457ae5b857604549309df2"
+        },
+        "date": 1604826400095,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 78.3018069267273,
+            "unit": "sec",
+            "range": "stddev: 1.3617879656816534",
+            "extra": "mean: 78.3018069267273 sec\nrounds: 3"
           }
         ]
       }
