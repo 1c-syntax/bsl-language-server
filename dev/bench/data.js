61b6ea0a1fc0696e4c2978185837bd85a26587c3
github-action-benchmark <github@users.noreply.github.com>
2021-01-21 06:40:43 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 4973f197b7c0d1ad30a69db29628f9a469632473

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611168521705,
+  "lastUpdate": 1611211243211,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -8308,6 +8308,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 1.590205256913211",
             "extra": "mean: 74.09491491317749 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Gryzlov",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4973f197b7c0d1ad30a69db29628f9a469632473",
+          "message": "Merge pull request #1529 from EightM/RefOverUseFix",
+          "timestamp": "2021-01-21T09:28:36+03:00",
+          "tree_id": "3a180a6b5cef6a48b3b66cf3450bbc9f09c7a431",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/4973f197b7c0d1ad30a69db29628f9a469632473"
+        },
+        "date": 1611211239875,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 72.70302700996399,
+            "unit": "sec",
+            "range": "stddev: 1.1416717197895432",
+            "extra": "mean: 72.70302700996399 sec\nrounds: 3"
           }
         ]
       }
