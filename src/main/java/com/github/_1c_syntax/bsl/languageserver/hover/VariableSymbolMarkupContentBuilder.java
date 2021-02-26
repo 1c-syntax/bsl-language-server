bcd7de9dafead9eae72ef2cb6d2d45c5533e1f24
Nikita Gryzlov <nixel2007@gmail.com>
2021-02-26 09:54:46 +0300

    Ховер для переменной с чтением описания

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/hover/VariableSymbolMarkupContentBuilder.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/hover/VariableSymbolMarkupContentBuilder.java
@@ -24,9 +24,12 @@ package com.github._1c_syntax.bsl.languageserver.hover;
 import com.github._1c_syntax.bsl.languageserver.configuration.LanguageServerConfiguration;
 import com.github._1c_syntax.bsl.languageserver.context.symbol.VariableSymbol;
 import com.github._1c_syntax.bsl.languageserver.context.symbol.variable.VariableDescription;
+import com.github._1c_syntax.bsl.languageserver.utils.MdoRefBuilder;
+import com.github._1c_syntax.bsl.languageserver.utils.Resources;
 import lombok.RequiredArgsConstructor;
 import org.eclipse.lsp4j.MarkupContent;
 import org.eclipse.lsp4j.MarkupKind;
+import org.eclipse.lsp4j.SymbolKind;
 import org.springframework.stereotype.Component;
 
 import java.util.StringJoiner;
@@ -35,20 +38,37 @@ import java.util.StringJoiner;
 @RequiredArgsConstructor
 public class VariableSymbolMarkupContentBuilder implements MarkupContentBuilder<VariableSymbol> {
 
+  private static final String VARIABLE_LOCATION_KEY = "variableLocation";
+  private static final String VARIABLE_KEY = "var";
+  private static final String EXPORT_KEY = "export";
+
   private final LanguageServerConfiguration configuration;
 
   @Override
   public MarkupContent getContent(VariableSymbol symbol) {
     var markupBuilder = new StringJoiner("\n");
 
+    // сигнатура
+    // местоположение переменной
+    // описание переменной
+
+    // сигнатура
+    String signature = getSignature(symbol);
+    addSectionIfNotEmpty(markupBuilder, signature);
+
+    // местоположение переменной
+    String location = getLocation(symbol);
+    addSectionIfNotEmpty(markupBuilder, location);
+
+    // описание переменной
     symbol.getDescription()
       .map(VariableDescription::getPurposeDescription)
-      .ifPresent(markupBuilder::add);
+      .ifPresent(description -> addSectionIfNotEmpty(markupBuilder, description));
 
     symbol.getDescription()
       .flatMap(VariableDescription::getTrailingDescription)
       .map(VariableDescription::getPurposeDescription)
-      .ifPresent(markupBuilder::add);
+      .ifPresent(trailingDescription -> addSectionIfNotEmpty(markupBuilder, trailingDescription));
 
     String content = markupBuilder.toString();
 
@@ -58,5 +78,46 @@ public class VariableSymbolMarkupContentBuilder implements MarkupContentBuilder<
   @Override
   public Class<VariableSymbol> getType() {
     return VariableSymbol.class;
+  }
+
+  private String getSignature(VariableSymbol symbol) {
+    String signatureTemplate = "```bsl\n%s %s%s\n```";
+
+    String varKey = getResourceString(VARIABLE_KEY);
+    String name = symbol.getName();
+    String export = symbol.isExport() ? (" " + getResourceString(EXPORT_KEY)) : "";
+
+    return String.format(
+      signatureTemplate,
+      varKey,
+      name,
+      export
+    );
+  }
+
+  private String getLocation(VariableSymbol symbol) {
+    String mdoRef = MdoRefBuilder.getMdoRef(symbol.getOwner());
+    String parentPostfix = symbol.getRootParent(SymbolKind.Method)
+      .map(sourceDefinedSymbol -> "." + sourceDefinedSymbol.getName())
+      .orElse("");
+
+    mdoRef += parentPostfix;
+    return getResourceString(VARIABLE_LOCATION_KEY, mdoRef);
+  }
+
+  private static void addSectionIfNotEmpty(StringJoiner markupBuilder, String newContent) {
+    if (!newContent.isEmpty()) {
+      markupBuilder.add(newContent);
+      markupBuilder.add("");
+      markupBuilder.add("---");
+    }
+  }
+
+  private String getResourceString(String key) {
+    return Resources.getResourceString(configuration.getLanguage(), getClass(), key);
+  }
+
+  private String getResourceString(String key, Object... args) {
+    return Resources.getResourceString(configuration.getLanguage(), getClass(), key, args);
   }
 }
