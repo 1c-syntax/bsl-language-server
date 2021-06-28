e0546a5fa881e8f36005fa3baa23338893581b5b
Chipazawra <Chipazawra@mail.ru>
2021-06-28 11:09:59 +0300

    add AssignAliasFieldsInQuery

---
+++ docs/en/diagnostics/AssignAliasFieldsInQuery.md
@@ -0,0 +1,36 @@
+# Assigning aliases to selected fields in a query (AssignAliasFieldsInQuery)
+
+ Type | Scope | Severity | Activated<br>by default | Minutes<br>to fix | Tags 
+ :-: | :-: | :-: | :-: | :-: | :-: 
+ `Code smell` | `BSL` | `Info` | `Yes` | `10` | `standard`<br>`sql`<br>`badpractice` 
+
+<!-- Блоки выше заполняются автоматически, не трогать -->
+## Description
+<!-- Описание диагностики заполняется вручную. Необходимо понятным языком описать смысл и схему работу -->
+
+## Examples
+<!-- В данном разделе приводятся примеры, на которые диагностика срабатывает, а также можно привести пример, как можно исправить ситуацию -->
+
+## Sources
+<!-- Необходимо указывать ссылки на все источники, из которых почерпнута информация для создания диагностики -->
+<!-- Примеры источников
+
+* Источник: [Стандарт: Тексты модулей](https://its.1c.ru/db/v8std#content:456:hdoc)
+* Полезная информация: [Отказ от использования модальных окон](https://its.1c.ru/db/metod8dev#content:5272:hdoc)
+* Источник: [Cognitive complexity, ver. 1.4](https://www.sonarsource.com/docs/CognitiveComplexity.pdf) -->
+
+## Snippets
+
+<!-- Блоки ниже заполняются автоматически, не трогать -->
+### Diagnostic ignorance in code
+
+```bsl
+// BSLLS:AssignAliasFieldsInQuery-off
+// BSLLS:AssignAliasFieldsInQuery-on
+```
+
+### Parameter for config
+
+```json
+"AssignAliasFieldsInQuery": false
+```
---
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/AssignAliasFieldsInQueryDiagnostic.java
@@ -0,0 +1,84 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright © 2018-2021
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Gryzlov <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package com.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticInfo;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticScope;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticTag;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
+import com.github._1c_syntax.bsl.languageserver.utils.Trees;
+import com.github._1c_syntax.bsl.parser.BSLParserRuleContext;
+import com.github._1c_syntax.bsl.parser.SDBLParser;
+import com.sun.source.tree.Tree;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+@DiagnosticMetadata(
+  type = DiagnosticType.CODE_SMELL,
+  severity = DiagnosticSeverity.INFO,
+  minutesToFix = 10,
+  scope = DiagnosticScope.BSL,
+  tags = {
+    DiagnosticTag.STANDARD,
+    DiagnosticTag.SQL,
+    DiagnosticTag.BADPRACTICE
+  }
+
+)
+public class AssignAliasFieldsInQueryDiagnostic extends AbstractSDBLVisitorDiagnostic {
+
+  @Override
+  public ParseTree visitAlias(SDBLParser.AliasContext ctx) {
+
+    var isValidationContext = ctx.getParent() instanceof SDBLParser.SelectedFieldContext
+      && Trees.getAncestorByRuleIndex(ctx, SDBLParser.RULE_union) == null;
+
+    if (isValidationContext && (ctx.identifier() == null || ctx.AS() == null)) {
+      diagnosticStorage.addDiagnostic(ctx);
+    }
+    return super.visitAlias(ctx);
+  }
+
+  @Override
+  public ParseTree visitQuery(SDBLParser.QueryContext ctx) {
+
+    if (ctx.getParent().getRuleIndex() == SDBLParser.RULE_subquery) {
+
+      for (SDBLParser.SelectedFieldContext selectedField :
+
+        ctx.selectedFields().selectedField()) {
+
+        var AliasContext = selectedField.alias();
+
+        if (AliasContext.identifier() == null || AliasContext.AS() == null) {
+          diagnosticStorage.addDiagnostic(ctx);
+        }
+
+      }
+
+
+      return super.visitQuery(ctx);
+    }
+
+
+  }
