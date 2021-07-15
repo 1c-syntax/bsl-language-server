040afb464a9e33d08da8fe2173ccff99240a9966
Andrey Ovsiankin <ovsiankin.a.a@gmail.com>
2021-07-15 11:11:44 +0300

    Сделал строгое API рерайтера, для устранения неоднозначностей по использованию

--- src/test/java/com/github/_1c_syntax/bsl/languageserver/utils/ExpressionTreeComparersTest.java
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/utils/ExpressionTreeComparersTest.java
@@ -72,10 +72,7 @@ class ExpressionTreeComparersTest {
 
   BslExpression getExpressionTree(String code) {
     var expression = parse(code);
-    var rewriter = new ExpressionParseTreeRewriter();
-    expression.accept(rewriter);
-
-    return rewriter.getExpressionTree();
+    return ExpressionParseTreeRewriter.buildExpressionTree(expression);
   }
 
 }
