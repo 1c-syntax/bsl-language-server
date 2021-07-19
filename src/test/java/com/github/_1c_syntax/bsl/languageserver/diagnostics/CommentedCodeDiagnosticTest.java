38ff53cb625e27d3d10652a7fe2ab14b2cc618ec
Konstantin <bapho.bush@gmail.com>
2021-07-19 22:53:30 +0300

    Обновлены тесты. Теперь для конфигурирования диагностики на вход подаются параметры в различных типах.

--- src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/CommentedCodeDiagnosticTest.java
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/CommentedCodeDiagnosticTest.java
@@ -27,6 +27,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import static com.github._1c_syntax.bsl.languageserver.util.Assertions.assertThat;
 
@@ -59,12 +60,19 @@ class CommentedCodeDiagnosticTest extends AbstractDiagnosticTest<CommentedCodeDi
   void testConfigure() {
 
     Map<String, Object> configuration = diagnosticInstance.info.getDefaultConfiguration();
-    configuration.put("threshold", 1f);
-    diagnosticInstance.configure(configuration);
 
-    List<Diagnostic> diagnostics = getDiagnostics();
+    Vector<Object> thresholdVariants = new Vector<>();
+    thresholdVariants.add(Float.valueOf(1f));
+    thresholdVariants.add(Double.valueOf(1));
+    thresholdVariants.add(Integer.valueOf(1));
 
-    assertThat(diagnostics).isEmpty();
+    for (Object threshold : thresholdVariants){
+      configuration.put("threshold", threshold);
+      diagnosticInstance.configure(configuration);
+
+      List<Diagnostic> diagnostics = getDiagnostics();
+      assertThat(diagnostics).isEmpty();
+    }
 
   }
 
