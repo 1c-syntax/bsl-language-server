8207fdbd27c06eb04a9a07346dd298515e2219b0
Nikita Gryzlov <nixel2007@gmail.com>
2021-08-03 11:46:04 +0300

    Тесты на ColorInformationSupplier

--- src/test/java/com/github/_1c_syntax/bsl/languageserver/util/Assertions.java
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/util/Assertions.java
@@ -22,12 +22,14 @@
 package com.github._1c_syntax.bsl.languageserver.util;
 
 import com.github._1c_syntax.bsl.languageserver.util.assertions.CodeActionAssert;
+import com.github._1c_syntax.bsl.languageserver.util.assertions.ColorInformationsAssert;
 import com.github._1c_syntax.bsl.languageserver.util.assertions.DiagnosticAssert;
 import com.github._1c_syntax.bsl.languageserver.util.assertions.DiagnosticsAssert;
 import com.github._1c_syntax.bsl.languageserver.util.assertions.FoldingRangeAssert;
 import com.github._1c_syntax.bsl.languageserver.util.assertions.FoldingRangesAssert;
 import com.github._1c_syntax.bsl.languageserver.util.assertions.SelectionRangesAssert;
 import org.eclipse.lsp4j.CodeAction;
+import org.eclipse.lsp4j.ColorInformation;
 import org.eclipse.lsp4j.Diagnostic;
 import org.eclipse.lsp4j.FoldingRange;
 import org.eclipse.lsp4j.SelectionRange;
@@ -58,5 +60,9 @@ public class Assertions extends org.assertj.core.api.Assertions {
 
   public static SelectionRangesAssert assertThatSelectionRanges(List<SelectionRange> actual) {
     return new SelectionRangesAssert(actual);
+  }
+
+  public static ColorInformationsAssert assertThatColorInformations(List<ColorInformation> actual) {
+    return new ColorInformationsAssert(actual);
   }
 }
