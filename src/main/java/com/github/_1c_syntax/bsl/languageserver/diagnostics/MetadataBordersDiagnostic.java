b17d97fd88bde71cf175e1fba95e5c9a028ed681
Golovanoff Sergio <s.golovanoff@gmail.com>
2021-09-21 12:17:31 +0700

    попытался накидать чтение настройки из json. но не взлетает

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/MetadataBordersDiagnostic.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/MetadataBordersDiagnostic.java
@@ -1,9 +1,10 @@
 package com.github._1c_syntax.bsl.languageserver.diagnostics;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.*;
 import com.github._1c_syntax.bsl.parser.*;
 import com.github._1c_syntax.utils.*;
-import com.google.protobuf.*;
 import org.antlr.v4.runtime.tree.*;
 
 import java.util.*;
@@ -20,18 +21,36 @@ import java.util.regex.*;
 )
 public class MetadataBordersDiagnostic extends AbstractVisitorDiagnostic {
 
-  private static final Map<String, String> METADATA_BORDERS_DEFAULT = new HashMap<>();
+  private static final String METADATA_BORDERS_DEFAULT = "{\"\":\"\"}";
 
   @DiagnosticParameter(
     type = String.class,
     defaultValue = ""
   )
-  private Map<String, String> metadataBordersParameters = METADATA_BORDERS_DEFAULT;
+  private Map<String, String> metadataBordersParameters = MapFromJSON(METADATA_BORDERS_DEFAULT);
+
+  private static HashMap<String, String> MapFromJSON(String userSettings) {
+    try {
+      return new ObjectMapper().readValue(userSettings, HashMap.class);
+    } catch (JsonProcessingException e) {
+      return new HashMap<>();
+    }
+  }
+
+  @Override
+  public void configure(Map<String, Object> configuration) {
+    this.metadataBordersParameters = MapFromJSON(
+      (String) configuration.getOrDefault("metadataBordersParameters", METADATA_BORDERS_DEFAULT));
+  }
 
   @Override
   public ParseTree visitStatement(BSLParser.StatementContext ctx){
 
     for (Map.Entry<String, String> entry: metadataBordersParameters.entrySet()) {
+
+      if (entry.getKey().trim().length() == 0) {
+        continue;
+      }
 
       Pattern pattern = CaseInsensitivePattern.compile(entry.getKey());
       Matcher matcher = pattern.matcher(ctx.getText());
--- src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/MetadataBordersDiagnosticTest.java
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/MetadataBordersDiagnosticTest.java
@@ -4,6 +4,7 @@ import org.eclipse.lsp4j.Diagnostic;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static com.github._1c_syntax.bsl.languageserver.util.Assertions.assertThat;
 
@@ -14,12 +15,27 @@ class MetadataBordersDiagnosticTest extends AbstractDiagnosticTest<MetadataBorde
 
   @Test
   void test() {
+    List<Diagnostic> diagnostics = getDiagnostics();
+    assertThat(diagnostics).hasSize(0);
+  }
+
+  @Test
+  void testConfigure() {
+
+    Map<String, Object> configuration = diagnosticInstance.info.getDefaultConfiguration();
+    configuration.put("metadataBordersParameters", "{\"лотус|шмотус\":\"MetadataBordersDiagnostic\"}");
+    diagnosticInstance.configure(configuration);
 
     List<Diagnostic> diagnostics = getDiagnostics();
 
-    assertThat(diagnostics).hasSize(1);
+    assertThat(diagnostics).hasSize(6);
     assertThat(diagnostics, true)
-      .hasRange(6, 0, 6, 20);
+      .hasRange(0, 42, 0, 47)
+      .hasRange(0, 48, 0, 54)
+      .hasRange(4, 4, 4, 9)
+      .hasRange(6, 24, 6, 29)
+      .hasRange(6, 34, 6, 39)
+      .hasRange(8, 4, 8, 10);
 
   }
 }
