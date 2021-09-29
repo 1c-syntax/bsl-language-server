8f2e749c77999acb6d489abb2d46f7c8fdb7fb29
github-action-benchmark <github@users.noreply.github.com>
2021-09-29 21:42:43 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 6b0d46d8498b2d9a234f156acffa093b4bb75c49

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632737396697,
+  "lastUpdate": 1632951763445,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -13454,6 +13454,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 2.0789644975430175",
             "extra": "mean: 75.96021215120952 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Gryzlov",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6b0d46d8498b2d9a234f156acffa093b4bb75c49",
+          "message": "Merge pull request #1842 from 1c-syntax/feature/modLineLength\n\nМодификация диагностики LineLengthDiagnostic",
+          "timestamp": "2021-09-30T00:34:18+03:00",
+          "tree_id": "51f332a9a7e658bf5d2a1225bdaf651db22a76c7",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/6b0d46d8498b2d9a234f156acffa093b4bb75c49"
+        },
+        "date": 1632951759172,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 78.57988667488098,
+            "unit": "sec",
+            "range": "stddev: 1.5211174134739434",
+            "extra": "mean: 78.57988667488098 sec\nrounds: 3"
           }
         ]
       }
