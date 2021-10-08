74b546b2a8f3e231dbb839260c77e30a4d178b10
github-action-benchmark <github@users.noreply.github.com>
2021-10-08 17:32:10 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 9f5c0562d39732c02ee604abf198099b046bf1b7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633713848061,
+  "lastUpdate": 1633714329705,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -13981,6 +13981,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 2.086810497722811",
             "extra": "mean: 74.69345792134602 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Gryzlov",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9f5c0562d39732c02ee604abf198099b046bf1b7",
+          "message": "Merge pull request #1823 from artbear/WrongMetadataInQuery",
+          "timestamp": "2021-10-08T20:18:28+03:00",
+          "tree_id": "fd88263f50a211211a286d81920d37b957bc01f0",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/9f5c0562d39732c02ee604abf198099b046bf1b7"
+        },
+        "date": 1633714326711,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 75.45040647188823,
+            "unit": "sec",
+            "range": "stddev: 4.687744962287618",
+            "extra": "mean: 75.45040647188823 sec\nrounds: 3"
           }
         ]
       }
