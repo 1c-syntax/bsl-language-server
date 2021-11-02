366797a17bd738cbf64d82c3e29a435e6ec7be74
github-action-benchmark <github@users.noreply.github.com>
2021-11-02 15:08:30 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 2775e87e1c888d304fbaa710cd05f3023e6a4329

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635684702365,
+  "lastUpdate": 1635865709704,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -14632,6 +14632,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 1.5139434728970365",
             "extra": "mean: 74.26941124598186 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Fedkin",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2775e87e1c888d304fbaa710cd05f3023e6a4329",
+          "message": "Merge pull request #1900 from 1c-syntax/fix/NpeWrongDataPathForFormElementsDiagnostic",
+          "timestamp": "2021-11-02T18:00:27+03:00",
+          "tree_id": "f9f7184fb708c691795534354bca0251966c9892",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/2775e87e1c888d304fbaa710cd05f3023e6a4329"
+        },
+        "date": 1635865705763,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 74.29987223943074,
+            "unit": "sec",
+            "range": "stddev: 0.46640130509900074",
+            "extra": "mean: 74.29987223943074 sec\nrounds: 3"
           }
         ]
       }
