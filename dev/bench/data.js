7ab997d1cc9e7eb0e6493a0a48ca2fc7b6aa8115
github-action-benchmark <github@users.noreply.github.com>
2022-05-20 08:41:49 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 288df9e7b21a052446b084e789a5c832164e258e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1653034030004,
+  "lastUpdate": 1653036109552,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -17391,6 +17391,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 0.5115608906065071",
             "extra": "mean: 43.00260837872823 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Fedkin",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "288df9e7b21a052446b084e789a5c832164e258e",
+          "message": "Merge pull request #2759 from EightM/feat/issue_439",
+          "timestamp": "2022-05-20T11:37:16+03:00",
+          "tree_id": "6136d888b90c2b27354231eb715a56569d1e1a23",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/288df9e7b21a052446b084e789a5c832164e258e"
+        },
+        "date": 1653036107023,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 42.858198165893555,
+            "unit": "sec",
+            "range": "stddev: 1.0239474884715325",
+            "extra": "mean: 42.858198165893555 sec\nrounds: 3"
           }
         ]
       }
