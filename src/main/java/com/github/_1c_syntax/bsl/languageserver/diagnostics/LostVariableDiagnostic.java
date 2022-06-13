4a558deb30072d9a74b7850a9a04dae16f2b6fbd
Artur Ayukhanov <aartbear@gmail.com>
2022-06-14 00:02:20 +0300

    реализация + новые падающие тест-кейсы

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/LostVariableDiagnostic.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/LostVariableDiagnostic.java
@@ -1,3 +1,24 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright (c) 2018-2022
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Fedkin <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
 package com.github._1c_syntax.bsl.languageserver.diagnostics;
 
 import com.github._1c_syntax.bsl.languageserver.context.symbol.VariableSymbol;
@@ -9,24 +30,23 @@ import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticT
 import com.github._1c_syntax.bsl.languageserver.references.ReferenceIndex;
 import com.github._1c_syntax.bsl.languageserver.references.model.OccurrenceType;
 import com.github._1c_syntax.bsl.languageserver.references.model.Reference;
-import com.github._1c_syntax.bsl.languageserver.utils.Ranges;
 import com.github._1c_syntax.bsl.languageserver.utils.RelatedInformation;
+import com.github._1c_syntax.bsl.languageserver.utils.Trees;
 import com.github._1c_syntax.bsl.parser.BSLParser;
+import com.github._1c_syntax.bsl.parser.BSLParserRuleContext;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.eclipse.lsp4j.Location;
+import org.antlr.v4.runtime.tree.TerminalNode;
 import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.Range;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -44,101 +64,78 @@ import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class LostVariableDiagnostic extends AbstractVisitorDiagnostic {
-  private final ReferenceIndex referenceIndex;
-  private List<VarData> variables;
 
-    private static final Set<VariableKind> CHECKING_VARIABLE_KINDS = EnumSet.of(
+  private static final Set<VariableKind> CHECKING_VARIABLE_KINDS = EnumSet.of(
 //    VariableKind.MODULE,
 //    VariableKind.LOCAL, // TODO учесть разные типы переменных
     VariableKind.DYNAMIC
   );
+  private final ReferenceIndex referenceIndex;
+
   @Value
-  private static class VarData implements Comparable {
-//    VariableSymbol symbol;
+  private static class VarData implements Comparable<VarData> {
     String name;
-    Range selectionRange;
-//    Reference definition;
+    Range defRange;
+    Range rewriteRange;
     List<Reference> references;
-    Status status = Status.NOT_CHECKED;
-//    List<List<Reference>> references;
 
     @Override
-    public int compareTo(@NotNull Object o) {
-      return compare(this.getSelectionRange(), ((VarData)o).getSelectionRange());
+    public int compareTo(@NotNull LostVariableDiagnostic.VarData o) {
+      return compare(this.getDefRange(), o.getDefRange());
     }
 
-//    public VarData(VariableSymbol symbol, List<Reference> references) {
-//      this.symbol = symbol;
-//      this.references = references.stream()
-//        .filter(ref -> ref.getOccurrenceType() == OccurrenceType.REFERENCE)
-//        .sorted((o1, o2) -> compare(o1.getSelectionRange().getStart(), o2.getSelectionRange().getStart()))
-//        .collect(Collectors.toList());
-//    }
-  }
-  private enum Status {
-    NOT_CHECKED,
-    ALL_REFS_INSIDE_ONE_BLOCK,
-    SOME_REFS_OUTSIDE_ONE_BLOCK
   }
 
   @Override
   public ParseTree visitFile(BSLParser.FileContext ctx) {
-    variables = documentContext.getSymbolTree().getVariables().stream()
+    documentContext.getSymbolTree().getVariables().stream()
       .filter(variable -> CHECKING_VARIABLE_KINDS.contains(variable.getKind()))
-      //.filter(variable -> !variable.isExport())
       .flatMap(variable -> getVarData(variable).stream())
-//      .filter(Objects::nonNull)
-      .sorted()
-//      .sorted((var1, var2) -> compare(var1.getSymbol().getSelectionRange(), var2.getSymbol().getSelectionRange()))
-      .collect(Collectors.toUnmodifiableList());
-    if (variables.isEmpty()) {
-      return defaultResult();
-    }
-    return super.visitFile(ctx);
+      .filter(this::isLostVariable)
+      .forEach(this::fireIssue);
+
+    return defaultResult();
   }
 
-  @Nullable
   private List<VarData> getVarData(VariableSymbol variable) {
     List<Reference> allReferences = getSortedReferencesByLocation(variable);
     if (allReferences.isEmpty()) {
       return Collections.emptyList();
     }
-    final var possibleLostReferences = hasConsecutiveDefinitions(variable, allReferences);
-    return possibleLostReferences.stream()
-      .map(references -> new VarData(variable.getName(), variable.getVariableNameRange(), references))
-      .collect(Collectors.toUnmodifiableList());
-//    if (possibleLostReferences.isEmpty()) {
-//      return null;
-//    }
-//    return new VarData(variable.getName(), variable.getVariableNameRange(), possibleLostReferences);
+    return getConsecutiveDefinitions(variable, allReferences);
   }
 
-  private List<List<Reference>> hasConsecutiveDefinitions(VariableSymbol variable, List<Reference> allReferences) {
-    List<List<Reference>> result = new ArrayList<>();
+  private List<Reference> getSortedReferencesByLocation(VariableSymbol variable) {
+    final var references = referenceIndex.getReferencesTo(variable);
+    return references.stream()
+      .sorted((o1, o2) -> compare(o1.getSelectionRange(), o2.getSelectionRange()))
+      .collect(Collectors.toList());
+  }
+
+  private List<VarData> getConsecutiveDefinitions(VariableSymbol variable, List<Reference> allReferences) {
+    List<VarData> result = new ArrayList<>();
     Reference prev = null;
-    if (allReferences.get(0).getOccurrenceType() == OccurrenceType.DEFINITION){
+    if (allReferences.get(0).getOccurrenceType() == OccurrenceType.DEFINITION) {
       prev = allReferences.get(0);
 
-      final var references = new ArrayList<Reference>(1 + allReferences.size());
-      references.add(Reference.of(variable.getParent().get(), variable,
-        new Location(documentContext.getUri().toString(), variable.getVariableNameRange()),
-          OccurrenceType.DEFINITION));
-      references.addAll(allReferences);
-      result.add(references);
-//      return true;
+      var references = allReferences.subList(1, allReferences.size());
+      var varData = new VarData(variable.getName(), variable.getVariableNameRange(),
+        allReferences.get(0).getSelectionRange(), references);
+      result.add(varData);
     }
-    for (int i = 1 ; i < allReferences.size(); i++) {
+    for (var i = 1; i < allReferences.size(); i++) {
       final var next = allReferences.get(i);
-      if (next.getOccurrenceType() == OccurrenceType.DEFINITION){
+      if (next.getOccurrenceType() == OccurrenceType.DEFINITION) {
         if (prev != null) {
-          final var references = new ArrayList<Reference>(2 + allReferences.size() - i);
-          references.add(prev);
-          references.add(next);
-          if (i < allReferences.size() - 1){
-            references.addAll(allReferences.subList(i + 1, allReferences.size()));
+          final List<Reference> references;
+          if (i < allReferences.size() - 1) {
+            references = allReferences.subList(i + 1, allReferences.size());
+          } else {
+            references = Collections.emptyList();
           }
-          result.add(references);
-//          return true;
+          var varData = new VarData(variable.getName(), prev.getSelectionRange(),
+            next.getSelectionRange(), references);
+          result.add(varData);
         }
         prev = next;
         continue;
@@ -148,76 +145,45 @@ public class LostVariableDiagnostic extends AbstractVisitorDiagnostic {
     return result;
   }
 
-  @NotNull
-  private List<Reference> getSortedReferencesByLocation(VariableSymbol variable) {
-    final var references = referenceIndex.getReferencesTo(variable);
-//    if (references.stream()
-//        .noneMatch(ref -> ref.getOccurrenceType() == OccurrenceType.REFERENCE)){
-//      return null;
-//    }
+  private boolean isLostVariable(VarData varData) {
+    // TODO ? быстрее найти сначала метод в дереве, а потом уже переменную в дерере метода?
+    //  чтобы постоянно не искать по всему дереву
+    final var defNode = Trees.findNodeContainsPosition(documentContext.getAst(),
+      varData.getDefRange().getStart());
+    if (defNode.isEmpty()) {
+      return false; // вдруг каким-то чудом все-таки не найдем )
+    }
+    var defCodeBlock = getCodeBlock(defNode);
+    final var rewriteNodeInsideDefCodeBlock = defCodeBlock
+      .flatMap(context -> Trees.findNodeContainsPosition(context,
+        varData.rewriteRange.getStart()));
+    if (rewriteNodeInsideDefCodeBlock.isEmpty()) {
+      return false;
+    }
+    var rewriteCodeBlock = getCodeBlock(rewriteNodeInsideDefCodeBlock);
+    if (defCodeBlock.get() != rewriteCodeBlock.get() && varData.references.isEmpty()) {
+      return false;
+    }
+    return defCodeBlock.get() == rewriteCodeBlock.get()
+      || rewriteCodeBlock
+        .filter(codeBlock -> hasReferenceOutsideRewriteBlock(varData.references, codeBlock))
+        .isEmpty();
+  }
+
+  private static Optional<BSLParser.CodeBlockContext> getCodeBlock(Optional<TerminalNode> defNode) {
+    return defNode
+      .map(TerminalNode::getParent)
+      .map(BSLParserRuleContext.class::cast)
+      .map(node -> Trees.getRootParent(node, BSLParser.RULE_codeBlock))
+      .filter(BSLParser.CodeBlockContext.class::isInstance)
+      .map(BSLParser.CodeBlockContext.class::cast);
+  }
+
+  private static boolean hasReferenceOutsideRewriteBlock(List<Reference> references, BSLParserRuleContext codeBlock) {
     return references.stream()
-      .sorted((o1, o2) -> compare(o1.getSelectionRange(), o2.getSelectionRange()))
-      .collect(Collectors.toList());
-  }
-
-  @Override
-  public ParseTree visitSubCodeBlock(BSLParser.SubCodeBlockContext ctx) {
-    final var blockRange = Ranges.create(ctx);
-    variables.stream()
-      //.filter(varData -> Ranges.containsRange(blockRange, varData.getSymbol().getSelectionRange()))
-//      .flatMap(varData -> isLostVars(blockRange, varData).stream())
-      .filter(varData -> isLostVars(blockRange, varData))
-      .forEach(varData -> fireIssue(varData));
-//      .collect(Collectors.toList());
-
-//    variables.stream()
-//      .filter(varData -> Ranges.containsRange(blockRange, varData.symbol.getRange()))
-//      .sorted((o1, o2) -> {
-//        final var range1 = o1.getSymbol().getRange().getStart();
-//        final var range2 = o2.getSymbol().getRange().getStart();
-//        return compare(range1, range2);
-//      })
-//      .collect(Collectors.toList());
-//    variables.stream()
-//      .filter(varData -> Ranges.containsRange(blockRange, varData.symbol.getRange()))
-//      .
-    return super.visitSubCodeBlock(ctx);
-  }
-
-  @Override
-  public ParseTree visitCodeBlock(BSLParser.CodeBlockContext ctx) {
-    final var parseTree = super.visitCodeBlock(ctx);
-    return parseTree;
-  }
-
-  private boolean isLostVars(Range blockRange, VarData varData) {
-    return varData.references
-      // TODO избавиться от 1го элемента, т.к. он дублируется в соседних полях VarData
-          .subList(1, varData.references.size()).stream()
-            .allMatch(reference -> Ranges.containsRange(blockRange, reference.getSelectionRange()));
-//      .map(references -> new VarData(varData.getName(), varData.getDefinition(), references))
-//      .collect(Collectors.toUnmodifiableList());
-//    return false;
-//    final var isVarDefIntoCodeBlock = Ranges.containsRange(blockRange, varData.getSymbol().getSelectionRange());
-//    final var references = varData.references;
-////    if (references.get(0).getOccurrenceType() == OccurrenceType.DEFINITION
-////      && ){
-////
-////    }
-//    for (int i = 0; i < references.size(); i++) {
-//      final var reference = references.get(i);
-//      if (reference.getOccurrenceType() != OccurrenceType.DEFINITION){
-//        continue;
-//      }
-//      if (i == references.size() - 1){
-//        return true;
-//      }
-//      //if (isAllNextRefsIntoCodeBlock(blockRange, reference, references.subList(i + 1, references.size() - 1)))
-//    }
-//    return false;
-////    return varData.getReferences()
-////      .stream()
-////      .allMatch(reference -> Ranges.containsRange(blockRange, reference.getSelectionRange()));
+      .map(Reference::getSelectionRange)
+      .anyMatch(range -> Trees.findNodeContainsPosition(codeBlock, range.getStart())
+        .isEmpty());
   }
 
   private static int compare(Range o1, Range o2) {
@@ -234,14 +200,8 @@ public class LostVariableDiagnostic extends AbstractVisitorDiagnostic {
       return 1;
     }
     // 1,4 1,9
-    if (pos1.getCharacter() < pos2.getCharacter()) {
-      return -1;
-    }
+    return Integer.compare(pos1.getCharacter(), pos2.getCharacter());
     // 1,9 1,4
-    if (pos1.getCharacter() > pos2.getCharacter()) {
-      return 1;
-    }
-    return 0;
   }
 
   private void fireIssue(VarData varData) {
@@ -252,5 +212,7 @@ public class LostVariableDiagnostic extends AbstractVisitorDiagnostic {
         "+1"
       )).collect(Collectors.toList());
     final var message = info.getMessage(varData.getName());
-    diagnosticStorage.addDiagnostic(varData.getSelectionRange(), message, relatedInformationList);
-  }}
+    diagnosticStorage.addDiagnostic(varData.getDefRange(), message);
+//    diagnosticStorage.addDiagnostic(varData.getDefRange(), message, relatedInformationList);
+  }
+}
--- src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/LostVariableDiagnosticTest.java
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/LostVariableDiagnosticTest.java
@@ -1,3 +1,24 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright (c) 2018-2022
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Fedkin <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
 package com.github._1c_syntax.bsl.languageserver.diagnostics;
 
 import com.github._1c_syntax.bsl.languageserver.util.CleanupContextBeforeClassAndAfterClass;
@@ -23,7 +44,11 @@ class LostVariableDiagnosticTest extends AbstractDiagnosticTest<LostVariableDiag
       .hasMessageOnRange(getMessage("Значение"), 1, 4, 12)
       .hasMessageOnRange(getMessage("МояПеременная"), 4, 4, 17)
       .hasMessageOnRange(getMessage("ТекстЗапроса"), 9, 4, 16)
-      .hasSize(3);
+      .hasMessageOnRange(getMessage("ТекстЗапроса"), 23, 4, 16)
+      .hasMessageOnRange(getMessage("ТекстЗапроса"), 31, 4, 16)
+      .hasMessageOnRange(getMessage("ТекстЗапроса"), 53, 7, 19)
+      .hasMessageOnRange(getMessage("ТекстЗапроса"), 74, 2, 14)
+      .hasSize(7);
     ;
 
   }
