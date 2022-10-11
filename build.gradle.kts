a88c892278774d0e7219649264688fd53bca16c8
Alexey Sosnoviy <int-it@yandex.ru>
2022-10-11 13:31:53 +0300

    bump bslparser

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,7 +70,7 @@ dependencies {
     api("org.eclipse.lsp4j", "org.eclipse.lsp4j", "0.14.0")
 
     // 1c-syntax
-    api("com.github.1c-syntax", "bsl-parser", "833015043ef98c5fa2303588a2f89fba991f97c2") {
+    api("com.github.1c-syntax", "bsl-parser", "167aaad827322e09ccde4658a71152dad234de4b") {
         exclude("com.tunnelvisionlabs", "antlr4-annotations")
         exclude("com.ibm.icu", "*")
         exclude("org.antlr", "ST4")
