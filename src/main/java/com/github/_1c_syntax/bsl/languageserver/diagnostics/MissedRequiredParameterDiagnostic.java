ed1080cdaa4b3c840a31690d6c9050883eacdbca
Корякин Алексей <aleksey.koryakin@bia-tech.ru>
2022-10-27 22:57:34 +0300

    Переделал для обработки вызова "внешних" методов

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/MissedRequiredParameterDiagnostic.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/MissedRequiredParameterDiagnostic.java
@@ -1,11 +1,22 @@
 package com.github._1c_syntax.bsl.languageserver.diagnostics;
 
+import com.github._1c_syntax.bsl.languageserver.context.symbol.MethodSymbol;
+import com.github._1c_syntax.bsl.languageserver.context.symbol.ParameterDefinition;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticTag;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
+import com.github._1c_syntax.bsl.languageserver.references.ReferenceIndex;
+import com.github._1c_syntax.bsl.languageserver.utils.Ranges;
 import com.github._1c_syntax.bsl.parser.BSLParser;
+import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.eclipse.lsp4j.Range;
+import org.eclipse.lsp4j.SymbolKind;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @DiagnosticMetadata(
   type = DiagnosticType.CODE_SMELL,
@@ -16,32 +27,76 @@ import org.antlr.v4.runtime.tree.ParseTree;
   }
 
 )
+@RequiredArgsConstructor
 public class MissedRequiredParameterDiagnostic extends AbstractVisitorDiagnostic {
+
+  private final ReferenceIndex referenceIndex;
+
+  Map<Range, MethodCall> calls = new HashMap<>();
+
+  @Override
+  public ParseTree visitFile(BSLParser.FileContext ctx) {
+    calls.clear();
+    super.visitFile(ctx);
+
+    for (var reference : referenceIndex.getReferencesFrom(documentContext.getUri(), SymbolKind.Method)) {
+      if (calls.containsKey(reference.getSelectionRange())) {
+        checkMethod((MethodSymbol) reference.getSymbol(), calls.get(reference.getSelectionRange()));
+      }
+    }
+
+    calls.clear();
+    return ctx;
+  }
 
   @Override
   public ParseTree visitGlobalMethodCall(BSLParser.GlobalMethodCallContext ctx) {
     String methodName = ctx.methodName().IDENTIFIER().getText();
-    var method = documentContext.getSymbolTree().getMethodSymbol(methodName);
-    if (method.isEmpty()) {
-      return ctx;
+    if (documentContext.getSymbolTree().getMethodSymbol(methodName).isPresent()) {
+      appendMethodCall(ctx.methodName().getStart(), ctx.doCall());
     }
 
-    var callParameters = ctx.doCall().callParamList().callParam();
-    var methodParameters = method.get().getParameters();
-    int callParametersCount = callParameters.size();
-    int methodParametersCount = methodParameters.size();
+    return super.visitGlobalMethodCall(ctx);
+  }
 
-    for (int i = 0; i < methodParametersCount; i++) {
-      var methodParameter = methodParameters.get(i);
+  @Override
+  public ParseTree visitMethodCall(BSLParser.MethodCallContext ctx) {
+    appendMethodCall(ctx.methodName().getStart(), ctx.doCall());
+    return super.visitMethodCall(ctx);
+  }
+
+  void appendMethodCall(Token methodName, BSLParser.DoCallContext doCallContext) {
+    var parameters = doCallContext.callParamList().callParam();
+    MethodCall methodCall = new MethodCall();
+    methodCall.parameters = new Boolean[parameters.size()];
+
+    for (int i = 0; i < methodCall.parameters.length; i++) {
+      methodCall.parameters[i] = parameters.get(i).expression() != null;
+    }
+
+    methodCall.range = Ranges.create(methodName);
+    calls.put(methodCall.range, methodCall);
+  }
+
+  private void checkMethod(MethodSymbol methodDefinition, MethodCall callInfo) {
+    int callParametersCount = callInfo.parameters.length;
+
+    ParameterDefinition methodParameter;
+    for (int i = 0; i < methodDefinition.getParameters().size(); i++) {
+
+      methodParameter = methodDefinition.getParameters().get(i);
       if (methodParameter.isOptional()) {
         continue;
       }
 
-      if (callParametersCount <= i || callParameters.get(i).expression() == null) {
-        diagnosticStorage.addDiagnostic(ctx, info.getMessage(methodParameter.getName()));
+      if (callParametersCount <= i || !callInfo.parameters[i]) {
+        diagnosticStorage.addDiagnostic(callInfo.range, info.getMessage(methodParameter.getName()));
       }
     }
+  }
 
-    return ctx;
+  private static class MethodCall {
+    Boolean[] parameters;
+    Range range;
   }
 }
--- src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/MissedRequiredParameterDiagnosticTest.java
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/MissedRequiredParameterDiagnosticTest.java
@@ -1,8 +1,14 @@
 package com.github._1c_syntax.bsl.languageserver.diagnostics;
 
+import com.github._1c_syntax.bsl.languageserver.util.TestUtils;
+import com.github._1c_syntax.utils.Absolute;
+import org.apache.commons.io.FileUtils;
 import org.eclipse.lsp4j.Diagnostic;
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static com.github._1c_syntax.bsl.languageserver.util.Assertions.assertThat;
@@ -11,12 +17,24 @@ class MissedRequiredParameterDiagnosticTest extends AbstractDiagnosticTest<Misse
   MissedRequiredParameterDiagnosticTest() {
     super(MissedRequiredParameterDiagnostic.class);
   }
+  private static final String PATH_TO_METADATA = "src/test/resources/metadata/designer";
 
   @Test
-  void test() {
+  void testLocalMethod() {
 
     List<Diagnostic> diagnostics = getDiagnostics();
 
+    assertThat(diagnostics).hasSize(4);
+    assertThat(diagnostics, true)
+      .hasRange(2, 16, 2, 29)
+      .hasRange(8, 16, 8, 27)
+      .hasRange(14, 16, 14, 26);
+  }
+
+  @Test
+  void testSideMethod() {
+    initServerContext(Absolute.path(PATH_TO_METADATA));
+    List<Diagnostic> diagnostics = getDiagnostics();
     assertThat(diagnostics).hasSize(4);
     assertThat(diagnostics, true)
       .hasRange(2, 16, 2, 29)
