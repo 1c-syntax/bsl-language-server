eda5a6385a75985d67c0cab34f0d81547d0d9339
Nikita Fedkin <nixel2007@gmail.com>
2023-05-13 12:47:42 +0200

    Тесты, что выполнение команды переключает инлэй хинты

---
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/commands/ToggleCognitiveComplexityInlayHintsCommandSupplierTest.java
@@ -0,0 +1,54 @@
+package com.github._1c_syntax.bsl.languageserver.commands;
+
+import com.github._1c_syntax.bsl.languageserver.commands.complexity.ToggleComplexityInlayHintsCommandArguments;
+import com.github._1c_syntax.bsl.languageserver.context.symbol.MethodSymbol;
+import com.github._1c_syntax.bsl.languageserver.inlayhints.CognitiveComplexityInlayHintSupplier;
+import com.github._1c_syntax.bsl.languageserver.util.CleanupContextBeforeClassAndAfterEachTestMethod;
+import com.github._1c_syntax.bsl.languageserver.util.TestUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+@CleanupContextBeforeClassAndAfterEachTestMethod
+class ToggleCognitiveComplexityInlayHintsCommandSupplierTest {
+  private final static String FILE_PATH = "./src/test/resources/commands/ToggleCognitiveComplexityInlayHintsCommandSupplier.bsl";
+
+  @MockBean
+  private CognitiveComplexityInlayHintSupplier complexityInlayHintSupplier;
+
+  @Autowired
+  private ToggleCognitiveComplexityInlayHintsCommandSupplier supplier;
+
+  @Test
+  void testExecute() {
+
+    // given
+    var documentContext = TestUtils.getDocumentContextFromFile(FILE_PATH);
+    MethodSymbol firstMethod = documentContext.getSymbolTree().getMethods().get(0);
+
+    var methodName = firstMethod.getName();
+
+    var arguments = new ToggleComplexityInlayHintsCommandArguments(
+      documentContext.getUri(),
+      supplier.getId(),
+      methodName
+    );
+
+    // when
+    Optional<Object> result = supplier.execute(arguments);
+
+    // then
+    assertThat(result).isEmpty();
+    verify(complexityInlayHintSupplier, times(1)).toggleHints(documentContext.getUri(), methodName);
+
+  }
+
+}
