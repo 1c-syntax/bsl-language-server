63140c52efd7129ab78a3a135a04fc16ac1aa083
Artur Ayukhanov <aartbear@gmail.com>
2023-06-22 18:16:26 +0300

    Реализация правила

---
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/ProtectedModuleDiagnostic.java
@@ -0,0 +1,86 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright (c) 2018-2023
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Fedkin <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package com.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticScope;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticTag;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
+import com.github._1c_syntax.bsl.languageserver.utils.MdoRefBuilder;
+import com.github._1c_syntax.bsl.mdo.ModuleOwner;
+import com.github._1c_syntax.bsl.types.MDOType;
+import com.github._1c_syntax.bsl.types.ModuleType;
+import com.github._1c_syntax.mdclasses.mdo.AbstractMDObjectBase;
+import com.github._1c_syntax.mdclasses.mdo.support.MDOModule;
+
+import java.util.List;
+
+@DiagnosticMetadata(
+  type = DiagnosticType.CODE_SMELL,
+  severity = DiagnosticSeverity.MAJOR,
+  minutesToFix = 5,
+  tags = {
+    DiagnosticTag.BADPRACTICE,
+    DiagnosticTag.SUSPICIOUS
+  },
+  modules = { ModuleType.SessionModule },
+  scope = DiagnosticScope.BSL
+)
+
+public class ProtectedModuleDiagnostic extends AbstractMetadataDiagnostic {
+
+  public ProtectedModuleDiagnostic() {
+    super(List.of(
+      MDOType.CONFIGURATION,
+      MDOType.ACCOUNTING_REGISTER,
+      MDOType.ACCUMULATION_REGISTER,
+      MDOType.BUSINESS_PROCESS,
+      MDOType.CALCULATION_REGISTER,
+      MDOType.CATALOG,
+      MDOType.CHART_OF_ACCOUNTS,
+      MDOType.CHART_OF_CALCULATION_TYPES,
+      MDOType.CHART_OF_CHARACTERISTIC_TYPES,
+      MDOType.COMMON_MODULE,
+      MDOType.CONSTANT,
+      MDOType.DOCUMENT,
+      MDOType.DOCUMENT_JOURNAL,
+      MDOType.ENUM,
+      MDOType.EXCHANGE_PLAN,
+      MDOType.FILTER_CRITERION,
+      MDOType.INFORMATION_REGISTER,
+      MDOType.TASK
+    ));
+  }
+
+  @Override
+  protected void checkMetadata(AbstractMDObjectBase mdo) {
+    if (!(mdo instanceof ModuleOwner)){
+      return;
+    }
+    var moduleOwner = (ModuleOwner) mdo;
+    moduleOwner.getProtectedModules().forEach((MDOModule mdoModule) ->{
+        var ownerMDOName = MdoRefBuilder.getLocaleOwnerMdoName(documentContext, mdo);
+        addDiagnostic(info.getMessage(ownerMDOName));
+      });
+  }
+}
