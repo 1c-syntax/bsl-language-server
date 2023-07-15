bc71ac5b566cf684fde24445007ade2996968338
Nikita Ivanchenko <n1k17ka@gmail.com>
2023-07-15 14:30:23 +0300

    Красынй тест

--- src/test/java/com/github/_1c_syntax/bsl/languageserver/codeactions/GenerateFunctionSupplierTest.java
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/codeactions/GenerateFunctionSupplierTest.java
@@ -1,7 +1,59 @@
 package com.github._1c_syntax.bsl.languageserver.codeactions;
 
+import com.github._1c_syntax.bsl.languageserver.configuration.Language;
+import com.github._1c_syntax.bsl.languageserver.configuration.LanguageServerConfiguration;
+import com.github._1c_syntax.bsl.languageserver.util.TestUtils;
+import com.github._1c_syntax.bsl.languageserver.utils.Ranges;
+import org.eclipse.lsp4j.CodeAction;
+import org.eclipse.lsp4j.CodeActionContext;
+import org.eclipse.lsp4j.CodeActionParams;
+import org.eclipse.lsp4j.Diagnostic;
+import org.eclipse.lsp4j.Range;
+import org.eclipse.lsp4j.TextDocumentIdentifier;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class GenerateFunctionSupplierTest {
+
+  @Autowired
+  private LanguageServerConfiguration configuration;
+
+  @Autowired
+  private GenerateFunctionSupplier codeActionSupplier;
+
+  @Test
+  void testGetCodeAction() {
+    // given
+    configuration.setLanguage(Language.EN);
+
+    String filePath = "./src/test/resources/suppliers/GenerateFunctionSupplier.bsl";
+    var documentContext = TestUtils.getDocumentContextFromFile(filePath);
+
+    List<Diagnostic> diagnostics = new ArrayList<>();
+
+    TextDocumentIdentifier textDocumentIdentifier = new TextDocumentIdentifier(documentContext.getUri().toString());
+
+    CodeActionContext codeActionContext = new CodeActionContext();
+    codeActionContext.setDiagnostics(diagnostics);
+
+    CodeActionParams params = new CodeActionParams();
+    params.setRange(Ranges.create(15 , 5, 34));
+    params.setTextDocument(textDocumentIdentifier);
+    params.setContext(codeActionContext);
+
+    // when
+    List<CodeAction> codeActions = codeActionSupplier.getCodeActions(params, documentContext);
+
+    assertThat(codeActions)
+      .hasSize(1)
+      .anyMatch(codeAction -> codeAction.getTitle().equals("Generate function"));
+  }
+
 }
