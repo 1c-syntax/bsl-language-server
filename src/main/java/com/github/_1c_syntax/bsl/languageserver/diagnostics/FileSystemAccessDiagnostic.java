5c3bb5bbc25921b7d485f0b6a447be82617c5c6f
Artur Ayukhanov <aartbear@gmail.com>
2023-07-25 19:15:11 +0300

    реализация правила FileSystemAccess

---
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/FileSystemAccessDiagnostic.java
@@ -0,0 +1,78 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright (c) 2018-2023
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Fedkin <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package com.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticScope;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticTag;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
+import com.github._1c_syntax.bsl.languageserver.utils.bsl.Constructors;
+import com.github._1c_syntax.bsl.parser.BSLParser;
+import com.github._1c_syntax.utils.CaseInsensitivePattern;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.util.regex.Pattern;
+
+@DiagnosticMetadata(
+  type = DiagnosticType.VULNERABILITY,
+  severity = DiagnosticSeverity.MINOR,
+  minutesToFix = 3,
+  tags = {
+    DiagnosticTag.SUSPICIOUS
+  },
+  scope = DiagnosticScope.BSL
+)
+public class FileSystemAccessDiagnostic extends AbstractFindMethodDiagnostic {
+  private static final Pattern NEW_EXPRESSION = CaseInsensitivePattern.compile(
+    "File|Файл|xBase|HTMLWriter|ЗаписьHTML|HTMLReader|ЧтениеHTML|FastInfosetReader|ЧтениеFastInfoset" +
+      "|FastInfosetWriter|ЗаписьFastInfoset|XSLTransform|ПреобразованиеXSL" +
+      "|ZipFileWriter|ЗаписьZipФайла|ZipFileReader|ЧтениеZipФайла|TextReader|ЧтениеТекста|TextWriter|ЗаписьТекста" +
+      "|TextExtraction|ИзвлечениеТекста|BinaryData|ДвоичныеДанные|FileStream|ФайловыйПоток");
+
+  private static final Pattern GLOBAL_METHODS = CaseInsensitivePattern.compile(
+    "ЗначениеВФайл|ValueToFile|КопироватьФайл|FileCopy|ОбъединитьФайлы|MergeFiles|ПереместитьФайл|MoveFile" +
+      "|РазделитьФайл|SplitFile|СоздатьКаталог|CreateDirectory|УдалитьФайлы|DeleteFiles" +
+      "|КаталогПрограммы|BinDir|КаталогВременныхФайлов|TempFilesDir|КаталогДокументов|DocumentsDir" +
+      "|РабочийКаталогДанныхПользователя|UserDataWorkDir");
+  // TODO добавить глобальные асинхронные методы работы с файлами
+
+  public FileSystemAccessDiagnostic() {
+    super(GLOBAL_METHODS);
+  }
+
+  @Override
+  public ParseTree visitNewExpression(BSLParser.NewExpressionContext ctx) {
+    Constructors.typeName(ctx).ifPresent((String typeName) -> {
+      var matcherTypeName = NEW_EXPRESSION.matcher(typeName);
+      if (matcherTypeName.matches()) {
+        diagnosticStorage.addDiagnostic(ctx);
+      }
+    });
+    return super.visitNewExpression(ctx);
+  }
+
+  @Override
+  protected boolean checkMethodCall(BSLParser.MethodCallContext ctx) {
+    return false;
+  }
+}
---
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/FileSystemAccessDiagnosticTest.java
@@ -0,0 +1,70 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright (c) 2018-2023
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Fedkin <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package com.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import org.eclipse.lsp4j.Diagnostic;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.github._1c_syntax.bsl.languageserver.util.Assertions.assertThat;
+
+class FileSystemAccessDiagnosticTest extends AbstractDiagnosticTest<FileSystemAccessDiagnostic> {
+  FileSystemAccessDiagnosticTest() {
+    super(FileSystemAccessDiagnostic.class);
+  }
+
+  @Test
+  void test() {
+
+    List<Diagnostic> diagnostics = getDiagnostics();
+
+    assertThat(diagnostics, true)
+      .hasRange(1, 15, 35)
+      .hasRange(2, 15, 41)
+      .hasRange(3, 15, 31)
+      .hasRange(4, 15, 31)
+      .hasRange(5, 15, 38)
+      .hasRange(6, 15, 38)
+      .hasRange(7, 15, 33)
+      .hasRange(8, 15, 44)
+      .hasRange(9, 15, 44)
+      .hasRange(10, 15, 41)
+      .hasRange(11, 15, 41)
+      .hasRange(12, 15, 45)
+      .hasRange(13, 15, 41)
+      .hasRange(14, 15, 56)
+      .hasRange(19, 15, 41)
+      .hasRange(24, 15, 26)
+
+      .hasRange(29, 4, 17)
+      .hasRange(30, 4, 18)
+      .hasRange(34, 4, 19)
+      .hasRange(36, 4, 19)
+      .hasRange(37, 4, 17)
+      .hasRange(38, 4, 18)
+      .hasRange(39, 4, 16)
+      .hasSize(23)
+    ;
+
+  }
+}
