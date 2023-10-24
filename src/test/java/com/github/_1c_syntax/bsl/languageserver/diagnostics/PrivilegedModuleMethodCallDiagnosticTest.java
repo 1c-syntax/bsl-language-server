73745fb3828de332feb8d0a670e35182b39f1a2c
Artur Ayukhanov <aartbear@gmail.com>
2023-10-24 13:19:19 +0300

    Реализация правила Обращение к методам привилегированных модулей
    добавлена отдельная мини-конфигурация для тестов
    
    closes #1870

---
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/PrivilegedModuleMethodCallDiagnosticTest.java
@@ -0,0 +1,56 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright (c) 2018-2023
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Fedkin <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package com.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import org.eclipse.lsp4j.Diagnostic;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.github._1c_syntax.bsl.languageserver.util.Assertions.assertThat;
+
+class PrivilegedModuleMethodCallDiagnosticTest extends AbstractDiagnosticTest<PrivilegedModuleMethodCallDiagnostic> {
+  private static final String PATH_TO_METADATA = "src/test/resources/metadata/privilegedModules";
+
+  PrivilegedModuleMethodCallDiagnosticTest() {
+    super(PrivilegedModuleMethodCallDiagnostic.class);
+  }
+
+  @Test
+  void test() {
+    initServerContext(PATH_TO_METADATA);
+
+    List<Diagnostic> diagnostics = getDiagnostics();
+
+    assertThat(diagnostics).hasSize(2);
+    assertThat(diagnostics, true)
+      .hasMessageOnRange("Проверьте обращение к методу ПубличнаяФункция привилегированного модуля", 3, 40, 56)
+      .hasMessageOnRange("Проверьте обращение к методу ПубличнаяПроцедура привилегированного модуля", 4, 29, 47);
+  }
+
+  @Test
+  void testWithoutMetadata() {
+
+    List<Diagnostic> diagnostics = getDiagnostics();
+    assertThat(diagnostics).isEmpty();
+  }
+}
