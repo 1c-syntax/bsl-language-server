99ddd7f09fd99acbcfe14d749236c3c7d738601a
Alexander Medvedko <alexmed2000@mail.ru>
2023-11-23 00:01:55 +0300

    Новая диагностика "Зарезервированные имена параметров"

---
+++ docs/en/diagnostics/ReservedParameterNames.md
@@ -0,0 +1,11 @@
+# Reserved parameter names (ReservedParameterNames)
+
+<!-- Блоки выше заполняются автоматически, не трогать -->
+## Description
+Parameter names should not contain reserved words such as system enumerations.
+The list of reserved words is set by a regular expression.
+The search is case-insensitive.
+
+**For example:**
+
+"FormGroupType|FormFieldType"
---
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/ReservedParameterNamesTest.java
@@ -0,0 +1,61 @@
+/*
+ * This file is a part of BSL Language Server.
+ *
+ * Copyright (c) 2018-2023
+ * Alexey Sosnoviy <labotamy@gmail.com>, Nikita Fedkin <nixel2007@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * BSL Language Server is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * BSL Language Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BSL Language Server.
+ */
+package com.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import org.eclipse.lsp4j.Diagnostic;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.github._1c_syntax.bsl.languageserver.util.Assertions.assertThat;
+
+class ReservedParameterNamesDiagnosticTest extends AbstractDiagnosticTest<ReservedParameterNamesDiagnostic> {
+  ReservedParameterNamesDiagnosticTest() {
+    super(ReservedParameterNamesDiagnostic.class);
+  }
+
+  @Test
+  void test() {
+
+    List<Diagnostic> diagnostics = getDiagnostics();
+    assertThat(diagnostics).hasSize(0); // Проверка количества
+
+  }
+
+  @Test
+  void testConfigure() {
+
+    Map<String, Object> configuration = diagnosticInstance.info.getDefaultConfiguration();
+    configuration.put("reservedWords", "ВидГруппыФормы");
+    diagnosticInstance.configure(configuration);
+
+    List<Diagnostic> diagnostics = getDiagnostics();
+
+    assertThat(diagnostics).hasSize(1);
+    assertThat(diagnostics, true)
+      .hasMessageOnRange("Необходимо переименовать параметр \"ВидГруппыФормы\", совпадающий с зарезервированным словом",
+        2, 16, 30);
+  
+  }
+        
+}
