1e5ca45c63a390c2ea42a7499b2d6ab3aeaef943
Nikita Fedkin <nixel2007@gmail.com>
2023-12-26 21:41:31 +0100

    Указал, что бины инфраструктурные.
    stringInterner инжектится внутрь DiagnosticBeanPostProcessor, поэтому во избежание ругани он должен быть инфраструктурным бином

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/infrastructure/UtilsConfiguration.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/infrastructure/UtilsConfiguration.java
@@ -22,19 +22,23 @@
 package com.github._1c_syntax.bsl.languageserver.infrastructure;
 
 import com.github._1c_syntax.utils.StringInterner;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
 
 /**
  * Конфигурация бинов из 1c-syntax/utils.
  */
 @Configuration
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class UtilsConfiguration {
 
   /**
    * @return Настроенный объект интернирователя строк.
    */
   @Bean
+  @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
   public StringInterner stringInterner() {
     return new StringInterner();
   }
