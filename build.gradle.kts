ac0866632a46860799b5ee7417ee1caf0923da68
Nikita Fedkin <nixel2007@gmail.com>
2024-06-23 07:29:43 +0200

    latest for spring boot links

--- build.gradle.kts
+++ build.gradle.kts
@@ -253,7 +253,7 @@ tasks.javadoc {
             "https://1c-syntax.github.io/bsl-parser/dev/javadoc",
             "https://1c-syntax.github.io/mdclasses/dev/javadoc",
             "https://javadoc.io/doc/org.antlr/antlr4-runtime/latest",
-            "https://docs.spring.io/spring-boot/3.3.0/api/java"
+            "https://docs.spring.io/spring-boot/api/java"
         )
     }
 }
