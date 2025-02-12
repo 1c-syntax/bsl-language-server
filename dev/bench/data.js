ba4b855a538e0c7972210c6e5f2f9ae1cc16c513
github-action-benchmark <github@users.noreply.github.com>
2025-02-12 20:28:56 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 26e155d23db22f5da9bce0e247524624310d8916

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739304045315,
+  "lastUpdate": 1739392136407,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -27683,6 +27683,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 0.9433521583792153",
             "extra": "mean: 79.97179651260376 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Fedkin",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "26e155d23db22f5da9bce0e247524624310d8916",
+          "message": "Merge pull request #3415 from 1c-syntax/fix/ask-for-send-event\n\nИсправлен пропуск вопроса перед отправкой в Sentry",
+          "timestamp": "2025-02-12T21:01:54+01:00",
+          "tree_id": "95d035d2cd60bbf03e53704b2485d6aa9e8fd313",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/26e155d23db22f5da9bce0e247524624310d8916"
+        },
+        "date": 1739392133064,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 80.75265955924988,
+            "unit": "sec",
+            "range": "stddev: 2.473718583219075",
+            "extra": "mean: 80.75265955924988 sec\nrounds: 3"
           }
         ]
       }
