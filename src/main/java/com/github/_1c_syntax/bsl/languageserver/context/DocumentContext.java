a9cd70dd0997b1222daac6eb8498e8f896606374
Nikita Fedkin <nixel2007@gmail.com>
2025-02-20 17:52:21 +0100

    Использование @Locked.Read для получения полей DocumentContext, которые могут быть в процессе перестройки

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/context/DocumentContext.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/context/DocumentContext.java
@@ -48,6 +48,7 @@ import edu.umd.cs.findbugs.annotations.Nullable;
 import jakarta.annotation.PostConstruct;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Locked;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -111,9 +112,9 @@ public class DocumentContext implements Comparable<DocumentContext> {
 
   @Getter
   private FileType fileType;
-  @Getter
+  @Getter(onMethod = @__({@Locked.Read("computeLock")}))
   private BSLTokenizer tokenizer;
-  @Getter
+  @Getter(onMethod = @__({@Locked.Read("computeLock")}))
   private SymbolTree symbolTree;
 
   @Getter
@@ -144,20 +145,24 @@ public class DocumentContext implements Comparable<DocumentContext> {
     return context;
   }
 
+  @Locked.Read("computeLock")
   public String getContent() {
     requireNonNull(content);
     return content;
   }
 
+  @Locked.Read("computeLock")
   public String[] getContentList() {
     return contentList.getOrCompute();
   }
 
+  @Locked.Read("computeLock")
   public BSLParser.FileContext getAst() {
     requireNonNull(content);
     return tokenizer.getAst();
   }
 
+  @Locked.Read("computeLock")
   public List<Token> getTokens() {
     requireNonNull(content);
     return tokenizer.getTokens();
@@ -173,6 +178,7 @@ public class DocumentContext implements Comparable<DocumentContext> {
       .toList();
   }
 
+  @Locked.Read("computeLock")
   public String getText(Range range) {
     Position start = range.getStart();
     Position end = range.getEnd();
