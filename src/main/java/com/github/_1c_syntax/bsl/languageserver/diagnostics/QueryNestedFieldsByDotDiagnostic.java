8fc4ef8a0067226da98c3b6a0f97bede07765e32
levon444 <levonbadasyan8@gmail.com>
2025-05-02 12:01:05 +0400

    Зафиксировал добавление шаблонных файлов проверки

---
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/QueryNestedFieldsByDotDiagnostic.java
@@ -0,0 +1,21 @@
+package com.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticTag;
+import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
+
+@DiagnosticMetadata(
+  type = DiagnosticType.CODE_SMELL,
+  severity = DiagnosticSeverity.INFO,
+  minutesToFix = 1,
+  tags = {
+    DiagnosticTag.SQL,
+    DiagnosticTag.PERFOMANCE,
+    DiagnosticTag.DESIGN
+  }
+
+)
+public class QueryNestedFieldsByDotDiagnostic extends AbstractVisitorDiagnostic {
+
+}
---
+++ src/test/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/QueryNestedFieldsByDotDiagnosticTest.java
@@ -0,0 +1,25 @@
+package com.github._1c_syntax.bsl.languageserver.diagnostics;
+
+import org.eclipse.lsp4j.Diagnostic;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.github._1c_syntax.bsl.languageserver.util.Assertions.assertThat;
+
+class QueryNestedFieldsByDotDiagnosticTest extends AbstractDiagnosticTest<QueryNestedFieldsByDotDiagnostic> {
+  QueryNestedFieldsByDotDiagnosticTest() {
+    super(QueryNestedFieldsByDotDiagnostic.class);
+  }
+
+  @Test
+  void test() {
+
+    List<Diagnostic> diagnostics = getDiagnostics();
+
+    assertThat(diagnostics).hasSize(1);
+    assertThat(diagnostics, true)
+      .hasRange(6, 0, 6, 20);
+
+  }
+}
