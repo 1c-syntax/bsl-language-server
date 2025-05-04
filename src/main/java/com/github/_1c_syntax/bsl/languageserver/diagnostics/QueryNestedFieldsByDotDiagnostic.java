15ada237a35d38eaa058abab9b10f03908896f0e
levon444 <levonbadasyan8@gmail.com>
2025-05-04 14:37:34 +0400

    Описана основная логика проверки для запросов, параметров виртуальных таблиц, условий приобразования типов, запросов в виртуальных таблицах и соединений

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/QueryNestedFieldsByDotDiagnostic.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/QueryNestedFieldsByDotDiagnostic.java
@@ -4,6 +4,11 @@ import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticM
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticTag;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
+import com.github._1c_syntax.bsl.parser.SDBLParser;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @DiagnosticMetadata(
   type = DiagnosticType.CODE_SMELL,
@@ -11,11 +16,47 @@ import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticT
   minutesToFix = 1,
   tags = {
     DiagnosticTag.SQL,
-    DiagnosticTag.PERFOMANCE,
+    DiagnosticTag.PERFORMANCE,
     DiagnosticTag.DESIGN
   }
 
 )
-public class QueryNestedFieldsByDotDiagnostic extends AbstractVisitorDiagnostic {
+public class QueryNestedFieldsByDotDiagnostic extends AbstractSDBLListenerDiagnostic {
 
+  public boolean isVirtualTable = false;
+  private final Set<ParseTree> columns = new HashSet<>();
+
+  @Override
+  public void enterQuery(SDBLParser.QueryContext ctx) {
+    isVirtualTable = false;
+    super.enterQuery(ctx);
+  }
+
+  @Override
+  public void exitVirtualTableParameter(SDBLParser.VirtualTableParameterContext ctx) {
+    isVirtualTable = true;
+    super.exitVirtualTableParameter(ctx);
+  }
+
+  @Override
+  public void enterCastFunction(SDBLParser.CastFunctionContext ctx) {
+    super.enterCastFunction(ctx);
+  }
+
+  @Override
+  public void enterFunctionCall(SDBLParser.FunctionCallContext ctx) {
+    if(ctx.identifier != null && ctx.columnNames.size() > 1){
+      diagnosticStorage.addDiagnostic(ctx);
+    }
+    super.enterFunctionCall(ctx);
+  }
+
+  @Override
+  public void enterColumn(SDBLParser.ColumnContext ctx) {
+
+    if((isVirtualTable && ctx.columnNames.size() == 1 && ctx.mdoName != null) || ctx.columnNames.size() > 1){
+      diagnosticStorage.addDiagnostic(ctx);
+    }
+    super.enterColumn(ctx);
+  }
 }
