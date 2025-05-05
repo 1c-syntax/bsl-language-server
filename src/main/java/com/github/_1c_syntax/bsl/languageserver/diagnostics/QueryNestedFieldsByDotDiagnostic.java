4f43a625bce0903a0d8eb8b525bca32abbb8e5eb
levon444 <levonbadasyan8@gmail.com>
2025-05-05 12:39:00 +0400

    Модуль  диагностики дополнен комментариями, оптимизированы условия регистрации проверок, сообщения проверки упрощено, исключено форматирование в связи с тем, что контекст, передаваемый в сообщение полностью аналогичен контексту диагностики

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/QueryNestedFieldsByDotDiagnostic.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/diagnostics/QueryNestedFieldsByDotDiagnostic.java
@@ -19,36 +19,40 @@ import com.github._1c_syntax.bsl.parser.SDBLParser;
 )
 public class QueryNestedFieldsByDotDiagnostic extends AbstractSDBLListenerDiagnostic {
 
+  //Флаг обработки параметров виртуальной таблицы
   public boolean isVirtualTable = false;
 
   @Override
   public void enterQuery(SDBLParser.QueryContext ctx) {
-    isVirtualTable = false;
+    isVirtualTable = false; //Сбрасываем флаг при начале обработки запроса
     super.enterQuery(ctx);
   }
 
   @Override
   public void exitVirtualTableParameter(SDBLParser.VirtualTableParameterContext ctx) {
-    isVirtualTable = true;
+    isVirtualTable = true; //Взводим флаг при начале обработки параметров виртуальной таблицы
     super.exitVirtualTableParameter(ctx);
   }
 
   @Override
   public void enterFunctionCall(SDBLParser.FunctionCallContext ctx) {
+    //Контролируем разыменование в функциях (ВЫРАЗИТЬ, ЕСТЬNULL и т.д.)
     if(ctx.identifier != null && ctx.columnNames.size() > 1){
-      diagnosticStorage.addDiagnostic(ctx,
-        info.getMessage(ctx.getText()));
+      diagnosticStorage.addDiagnostic(ctx);
     }
     super.enterFunctionCall(ctx);
   }
 
   @Override
   public void enterColumn(SDBLParser.ColumnContext ctx) {
+    /*Если взведен флаг обработки виртуальной таблицы
+    и определен контекст метаданных, то проверяем заполненность контекста имен колонок.
+    В противном случае считаем что работаем со стандартным полем выборки или соединения
+    и выводим ошибку когда список имен колонки содержит более одного идентификатора
+    */
+    if((isVirtualTable && ctx.mdoName != null && !ctx.columnNames.isEmpty()) || ctx.columnNames.size() > 1){
 
-    if((isVirtualTable && ctx.columnNames.size() == 1 && ctx.mdoName != null) || ctx.columnNames.size() > 1){
-
-      diagnosticStorage.addDiagnostic(ctx,
-        info.getMessage(ctx.getText()));
+      diagnosticStorage.addDiagnostic(ctx);
     }
     super.enterColumn(ctx);
   }
