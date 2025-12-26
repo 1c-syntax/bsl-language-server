1f90afe1ac8d9199e05081984c4ff75c8781bd9c
github-action-benchmark <github@users.noreply.github.com>
2025-12-26 19:24:44 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 655b63722bcd85c642821b048a538cc515530ecc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766748422540,
+  "lastUpdate": 1766777084157,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -32953,6 +32953,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 0.44173437957644107",
             "extra": "mean: 81.37597036361694 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Fedkin",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "655b63722bcd85c642821b048a538cc515530ecc",
+          "message": "Merge pull request #3704 from 1c-syntax/copilot/optimize-incremental-sync\n\nOptimize incremental text document synchronization",
+          "timestamp": "2025-12-26T20:15:22+01:00",
+          "tree_id": "0032d01aae9ffa44d6d95280641dfe74d0509159",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/655b63722bcd85c642821b048a538cc515530ecc"
+        },
+        "date": 1766777078011,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 81.04225039482117,
+            "unit": "sec",
+            "range": "stddev: 1.2496616298388474",
+            "extra": "mean: 81.04225039482117 sec\nrounds: 3"
           }
         ]
       }
