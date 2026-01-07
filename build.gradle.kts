bb3d0faaeb4a328795ca8322f3b1e489bd11300b
Nikita Fedkin <nixel2007@gmail.com>
2026-01-07 08:37:39 +0100

    Replace license plugin with Spotless and update license header format

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     `java-library`
     `maven-publish`
     jacoco
-    id("cloud.rio.license") version "0.18.0"
+    id("com.diffplug.spotless") version "7.0.4"
     id("me.qoomon.git-versioning") version "6.4.4"
     id("io.freefair.lombok") version "9.1.0"
     id("io.freefair.javadoc-links") version "9.1.0"
@@ -38,21 +38,21 @@ gitVersioning.apply {
     refs {
         describeTagFirstParent = false
         tag("v(?<tagVersion>[0-9].*)") {
-            version = "\${ref.tagVersion}\${dirty}"
+            version = $$"${ref.tagVersion}${dirty}"
         }
 
         branch("develop") {
-            version = "\${describe.tag.version}." +
-                    "\${describe.distance}-SNAPSHOT\${dirty}"
+            version = $$"${describe.tag.version}." +
+                    $$"${describe.distance}-SNAPSHOT${dirty}"
         }
 
         branch(".+") {
-            version = "\${ref}-\${commit.short}\${dirty}"
+            version = $$"${ref}-${commit.short}${dirty}"
         }
     }
 
     rev {
-        version = "\${commit.short}\${dirty}"
+        version = $$"${commit.short}${dirty}"
     }
 }
 
@@ -61,12 +61,8 @@ gitProperties {
 }
 
 val languageToolVersion = "6.7"
-val sentryVersion = "8.29.0"
 
 dependencies {
-
-    // BOM
-//    api(platform("io.sentry:sentry-bom:$sentryVersion"))
 
     // RUNTIME
 
@@ -197,9 +193,16 @@ tasks.bootJar {
     archiveClassifier.set("exec")
 }
 
-tasks.named("sourcesJar") {
-    dependsOn(tasks.generateSentryDebugMetaPropertiesjava)
-    dependsOn(tasks.collectExternalDependenciesForSentry)
+afterEvaluate {
+    tasks.named("spotlessJavaCheck") {
+        dependsOn(tasks.generateSentryDebugMetaPropertiesjava)
+        dependsOn(tasks.collectExternalDependenciesForSentry)
+    }
+
+    tasks.named("spotlessJavaApply") {
+        dependsOn(tasks.generateSentryDebugMetaPropertiesjava)
+        dependsOn(tasks.collectExternalDependenciesForSentry)
+    }
 }
 
 tasks.build {
@@ -250,16 +253,6 @@ tasks.test {
 tasks.check {
     dependsOn(tasks.jacocoTestReport)
     mustRunAfter(tasks.generateDiagnosticDocs)
-}
-
-tasks.named("licenseMain") {
-    dependsOn(tasks.generateSentryDebugMetaPropertiesjava)
-    dependsOn(tasks.collectExternalDependenciesForSentry)
-}
-
-tasks.named("licenseFormatMain") {
-    dependsOn(tasks.generateSentryDebugMetaPropertiesjava)
-    dependsOn(tasks.collectExternalDependenciesForSentry)
 }
 
 tasks.jacocoTestReport {
@@ -315,23 +308,11 @@ tasks.javadoc {
     }
 }
 
-license {
-    header = rootProject.file("license/HEADER.txt")
-    skipExistingHeaders = false
-    strictCheck = true
-    ext["year"] = "2018-" + Calendar.getInstance().get(Calendar.YEAR)
-    ext["name"] = "Alexey Sosnoviy <labotamy@gmail.com>, Nikita Fedkin <nixel2007@gmail.com>"
-    ext["project"] = "BSL Language Server"
-    mapping("java", "SLASHSTAR_STYLE")
-    exclude("**/*.properties")
-    exclude("**/*.xml")
-    exclude("**/*.json")
-    exclude("**/*.bsl")
-    exclude("**/*.os")
-    exclude("**/*.txt")
-    exclude("**/*.java.orig")
-    exclude("**/*.impl")
-    exclude("**/*.mockito.plugins.MockMaker")
+spotless {
+    java {
+        targetExclude("**/AbstractObjectPool.java")
+        licenseHeaderFile(rootProject.file("license/HEADER.txt"), "package ").updateYearWithLatest(true)
+    }
 }
 
 sonarqube {
@@ -461,9 +442,9 @@ tasks.withType<GenerateModuleMetadata> {
 }
 
 tasks.register("updateLicenses") {
-    description = "Wrapper for licenseFormat"
+    description = "Wrapper for spotlessApply"
     group = "license"
-    dependsOn(tasks.licenseFormat)
+    dependsOn(tasks.spotlessApply)
 }
 
 fun buildTime(): String {
