df275f294ce6cdb3baa1fc3249c1a740a656b624
github-action-benchmark <github@users.noreply.github.com>
2026-01-17 16:59:12 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 424c14f3d099771811cecb02aaee09afb61ee1cc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768585706366,
+  "lastUpdate": 1768669152297,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -34503,6 +34503,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 1.1127722051512876",
             "extra": "mean: 69.9358736673991 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Fedkin",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "424c14f3d099771811cecb02aaee09afb61ee1cc",
+          "message": "Merge pull request #3785 from 1c-syntax/copilot/fix-illegal-argument-exception\n\nFix IllegalArgumentException when LSP clients send didChangeConfiguration without settings",
+          "timestamp": "2026-01-17T17:50:25+01:00",
+          "tree_id": "07c8f79e721dbc3f257d213c860a969e68997f98",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/424c14f3d099771811cecb02aaee09afb61ee1cc"
+        },
+        "date": 1768669146577,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 69.22712016105652,
+            "unit": "sec",
+            "range": "stddev: 1.24710148495493",
+            "extra": "mean: 69.22712016105652 sec\nrounds: 3"
           }
         ]
       }
