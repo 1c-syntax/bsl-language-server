839a37e6b66eff8ac5b348ce82908b56b8fbc2f8
github-action-benchmark <github@users.noreply.github.com>
2026-01-21 16:13:07 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 895b9c7c8eb03ae4a9ce384a54579a672b9ad39b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768823249554,
+  "lastUpdate": 1769011986898,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -34596,6 +34596,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 0.3630730496622009",
             "extra": "mean: 69.57523488998413 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Fedkin",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "895b9c7c8eb03ae4a9ce384a54579a672b9ad39b",
+          "message": "Merge pull request #3790 from sfaqer/bugfix/QueryNestedFieldsByDotDiagnosticTest\n\nИсправлена ошибка в диагностике QueryNestedFieldsByDotDiagnostic",
+          "timestamp": "2026-01-21T17:04:19+01:00",
+          "tree_id": "4e1bdb699bc7d6037aa4afb850fdeb38d079f000",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/895b9c7c8eb03ae4a9ce384a54579a672b9ad39b"
+        },
+        "date": 1769011981404,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 69.55678637822469,
+            "unit": "sec",
+            "range": "stddev: 0.4500193783861431",
+            "extra": "mean: 69.55678637822469 sec\nrounds: 3"
           }
         ]
       }
