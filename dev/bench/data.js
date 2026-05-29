172a71e44cc4766514ad0a8a1b95238ac00ebbb9
github-action-benchmark <github@users.noreply.github.com>
2026-05-29 01:34:14 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for c3ae226c3a5bd6487e0c431c3c4dbc1482a1f8ac

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780005501985,
+  "lastUpdate": 1780018453512,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -35836,6 +35836,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 12.076646542932846",
             "extra": "mean: 387.79813663164776 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Fedkin",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c3ae226c3a5bd6487e0c431c3c4dbc1482a1f8ac",
+          "message": "Merge pull request #3965 from 1c-syntax/dependabot/gradle/io.github.1c-syntax-bsl-common-library-0.10.1\n\nbuild(deps): bump io.github.1c-syntax:bsl-common-library from 0.10.0 to 0.10.1",
+          "timestamp": "2026-05-28T18:15:09+02:00",
+          "tree_id": "b6a53f17d7551461ba5ae0fc4589ebd79aefbb5f",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/c3ae226c3a5bd6487e0c431c3c4dbc1482a1f8ac"
+        },
+        "date": 1780018450994,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 372.41944710413617,
+            "unit": "sec",
+            "range": "stddev: 6.560286934159311",
+            "extra": "mean: 372.41944710413617 sec\nrounds: 3"
           }
         ]
       }
