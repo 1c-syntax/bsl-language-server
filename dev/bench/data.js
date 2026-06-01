c909cb36f6f20e093bd10eb97d1a5239b8e271be
github-action-benchmark <github@users.noreply.github.com>
2026-06-01 20:56:40 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 3fb5c77bbbde22dedf21456515728af2f11fd3ea

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780318589057,
+  "lastUpdate": 1780347400356,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -36084,6 +36084,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 65.75567416001971",
             "extra": "mean: 1186.858162164688 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Fedkin",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3fb5c77bbbde22dedf21456515728af2f11fd3ea",
+          "message": "Merge pull request #3985 from 1c-syntax/feature/hover-optional-params-and-structure-render\n\nHover/signature/автокомплит: «?» у необязательных параметров и список содержимого структур",
+          "timestamp": "2026-06-01T13:29:40+02:00",
+          "tree_id": "ea8085a14830535e2a5ae1d741b0c492916fe846",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/3fb5c77bbbde22dedf21456515728af2f11fd3ea"
+        },
+        "date": 1780347392132,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 1374.8199602762859,
+            "unit": "sec",
+            "range": "stddev: 42.06362442051337",
+            "extra": "mean: 1374.8199602762859 sec\nrounds: 3"
           }
         ]
       }
