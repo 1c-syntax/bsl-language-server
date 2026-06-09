9a8d9f93f8d8820453495764616976871e5908b5
github-action-benchmark <github@users.noreply.github.com>
2026-06-09 09:01:05 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 5030755a3128e1f5e0cc87af2765b3368f1a2b74

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780985965973,
+  "lastUpdate": 1780995664693,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -36642,6 +36642,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 1.4500243084408933",
             "extra": "mean: 270.17723353703815 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Fedkin",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5030755a3128e1f5e0cc87af2765b3368f1a2b74",
+          "message": "Merge pull request #4023 from 1c-syntax/fix/deprecated-version-normalization-and-chart-fallback\n\nfix(diagnostics): нормализация двухкомпонентных версий + чистка хардкода Deprecated 8.3.12",
+          "timestamp": "2026-06-09T08:04:16+02:00",
+          "tree_id": "0967962775e0afbec5658d94ed4c6bced8b51600",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/5030755a3128e1f5e0cc87af2765b3368f1a2b74"
+        },
+        "date": 1780995655386,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 278.39867345492047,
+            "unit": "sec",
+            "range": "stddev: 5.345843017426163",
+            "extra": "mean: 278.39867345492047 sec\nrounds: 3"
           }
         ]
       }
