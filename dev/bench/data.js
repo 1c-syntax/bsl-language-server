9c282e6bd3970886d86d7c908700996ad00f170a
github-action-benchmark <github@users.noreply.github.com>
2026-07-24 12:33:36 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 508e8cdfae824607fae92e5c99c2b43689d10e88

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784831408174,
+  "lastUpdate": 1784896415699,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -40672,6 +40672,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 2.0055729107500686",
             "extra": "mean: 91.22408898671468 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Fedkin",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "508e8cdfae824607fae92e5c99c2b43689d10e88",
+          "message": "Merge pull request #4297 from 1c-syntax/fix/early-config-type-registration\n\nfix(types): регистрировать конфигурационные типы до построения деревьев символов + регистрация членов объектов Отчетов и Обработок",
+          "timestamp": "2026-07-24T14:20:10+02:00",
+          "tree_id": "0d84213e90e4cdb64da1e1d793ca03173154c86e",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/508e8cdfae824607fae92e5c99c2b43689d10e88"
+        },
+        "date": 1784896402844,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 94.07799585660298,
+            "unit": "sec",
+            "range": "stddev: 2.6597846702559536",
+            "extra": "mean: 94.07799585660298 sec\nrounds: 3"
           }
         ]
       }
