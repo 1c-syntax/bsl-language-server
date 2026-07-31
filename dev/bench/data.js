912caa42fe8b10279b5a9e97d707cd9860933e9e
github-action-benchmark <github@users.noreply.github.com>
2026-07-31 12:32:14 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 95babf8db586adccc4c2c23afaba95e34368eb38

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785497469184,
+  "lastUpdate": 1785501133615,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -41416,6 +41416,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 2.392516187306287",
             "extra": "mean: 93.61377851168315 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Fedkin",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "95babf8db586adccc4c2c23afaba95e34368eb38",
+          "message": "Merge pull request #4341 from 1c-syntax/fix/flow-miss-is-an-error\n\nrefactor(types): срыв расчёта по потоку виден в журнале, а не подменяется типом",
+          "timestamp": "2026-07-31T12:11:39+02:00",
+          "tree_id": "6800c240b023eaa9fdab973694c44acfee6d769e",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/95babf8db586adccc4c2c23afaba95e34368eb38"
+        },
+        "date": 1785501122179,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 94.64916427930196,
+            "unit": "sec",
+            "range": "stddev: 1.743354546947724",
+            "extra": "mean: 94.64916427930196 sec\nrounds: 3"
           }
         ]
       }
