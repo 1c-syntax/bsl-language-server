f25080e531979ffbae84d2ccd3f748c0701c05a8
github-action-benchmark <github@users.noreply.github.com>
2026-08-02 16:43:21 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 7bb1457be2add282f281c12c6571333175160f32

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785687501303,
+  "lastUpdate": 1785689000776,
   "repoUrl": "https://github.com/1c-syntax/bsl-language-server",
   "entries": {
     "BSL LS perfomance measurement (SSL 3.1)": [
@@ -41881,6 +41881,37 @@ window.BENCHMARK_DATA = {
             "unit": "sec",
             "range": "stddev: 1.8734386902654474",
             "extra": "mean: 97.19272462526958 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Fedkin",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7bb1457be2add282f281c12c6571333175160f32",
+          "message": "Merge pull request #4379 from 1c-syntax/fix/types-element-types-of-call\n\nfix(types): тип элемента коллекции доезжает до возвращаемого значения метода",
+          "timestamp": "2026-08-02T16:36:28+02:00",
+          "tree_id": "cc34578f355e4dcf3fc4193256c8468884983ef1",
+          "url": "https://github.com/1c-syntax/bsl-language-server/commit/7bb1457be2add282f281c12c6571333175160f32"
+        },
+        "date": 1785688988776,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 96.85683870315552,
+            "unit": "sec",
+            "range": "stddev: 2.592416483839355",
+            "extra": "mean: 96.85683870315552 sec\nrounds: 3"
           }
         ]
       }
